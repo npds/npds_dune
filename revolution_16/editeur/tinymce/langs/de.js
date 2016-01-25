1d7d878842bcc2a03d99ff84bd14a2a4ac570ec6
jpb <jean.barbary@wanadoo.fr>
2016-01-25 19:05:55 +0100

    raz lang
    remise à origine fichier langs suppression des ressources lang NPDS qui
    sont dans le plugin

--- revolution_16/editeur/tinymce/langs/de.js
+++ revolution_16/editeur/tinymce/langs/de.js
@@ -215,22 +215,5 @@ tinymce.addI18n('de',{
 "Tools": "Werkzeuge",
 "View": "Ansicht",
 "Table": "Tabelle",
-"Format": "Format",
-"npds_img_desc": "Bilder Bank",
-"npds_perso_desc": "Ihre Bilder und Dokumente",
-"npds_mns_desc": "MiniSite",
-"npds_upl_desc": "Downloads",
-"npds_metal_desc": "Meta-Lang",
-"npds_plug_desc": "Zus‰tzliche Modules",
-"npds_what_desc": "Sprache w‰hlen",
-"npds_french_desc": "Franzˆsisch",
-"npds_frenchonly_desc": "Nur Franzˆsisch",
-"npds_english_desc": "Englisch",
-"npds_englishonly_desc": "Nur Englisch",
-"npds_spanish_desc": "Spanisch",
-"npds_spanishonly_desc": "Nur Spanisch",
-"npds_chinese_desc": "Chinese",
-"npds_chineseonly_desc": "Nur Chinese",
-"npds_german_desc": "Deutsch",
-"npds_germanonly_desc": "Nur Deutsch"
+"Format": "Format"
 });
--- revolution_16/editeur/tinymce/langs/en.js
+++ revolution_16/editeur/tinymce/langs/en.js
@@ -175,22 +175,5 @@ tinymce.addI18n('en_GB',{
 "Tools": "Tools",
 "View": "View",
 "Table": "Table",
-"Format": "Format",
-"npds_img_desc": "Pictures bank",
-"npds_perso_desc": "Your pictures and documents",
-"npds_mns_desc": "MiniSite",
-"npds_upl_desc": "Download",
-"npds_metal_desc": "Meta-Lang",
-"npds_plug_desc": "Additional modules",
-"npds_what_desc": "Choose language",
-"npds_french_desc": "french",
-"npds_frenchonly_desc": "french only",
-"npds_english_desc": "english",
-"npds_englishonly_desc": "english only",
-"npds_spanish_desc": "Spanish",
-"npds_spanishonly_desc": "Only spanish",
-"npds_chinese_desc": "Chinese",
-"npds_chineseonly_desc": "Only chinese",
-"npds_german_desc": "German",
-"npds_germanonly_desc": "Only german"
+"Format": "Format"
 });
