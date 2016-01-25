dbc71fdd47a68206bd1bfae0ef031acc1d8da9f9
jpb <jean.barbary@wanadoo.fr>
2016-01-25 19:06:45 +0100

    ajust hauteur volet bas pour edge

--- revolution_16/chat.php
+++ revolution_16/chat.php
@@ -11,20 +11,20 @@
 /************************************************************************/
 // Pour le lancement du Chat : chat.php?id=gp_id&auto=token_de_securite
 // gp_id=ID du groupe au sens NPDS du terme => 0 : tous / -127 : Admin / -1 : Anonyme / 1 : membre / 2 ... 126 : groupe de membre
-// token_de_securite = encrypt(serialize(gp_id)) => Permet d'éviter le lancement du Chat sans autorisation
+// token_de_securite = encrypt(serialize(gp_id)) => Permet d'Ã©viter le lancement du Chat sans autorisation
 
    if (!defined('NPDS_GRAB_GLOBALS_INCLUDED'))
       include("grab_globals.php");
 
-   $Titlesitename="NPDS";
-   $nuke_url="";
-   $meta_op="";
+   $Titlesitename='NPDS';
+   $nuke_url='';
+   $meta_op='';
    $meta_doctype="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset///EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">";
    include("meta/meta.php");
    echo '
          <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
       </head>
-      <frameset rows="1%,60%,160">
+      <frameset rows="1%,60%,200">
          <frame src="chatrafraich.php?repere=0&amp;aff_entetes=1&amp;connectes=-1&amp;id=$id&amp;auto='.$auto.'" frameborder="0" scrolling="no" noresize="noresize" name="rafraich">
          <frame src="chattop.php" frameborder="0" scrolling="yes" noresize="noresize" name="haut">
          <frame src="chatinput.php?id=$id&amp;auto='.$auto.'" frameborder="0" scrolling="no" noresize="noresize" name="bas">
