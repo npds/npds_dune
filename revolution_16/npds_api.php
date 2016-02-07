d73b9a2fc20e1f0b5445b349adb71e909bb341b2
jpb <jean.barbary@wanadoo.fr>
2016-02-07 14:28:05 +0100

    inside code SQL
    maintenance

--- revolution_16/npds_api.php
+++ revolution_16/npds_api.php
@@ -4,8 +4,9 @@
 /* ===========================                                          */
 /*                                                                      */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2012 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2015 by Philippe Brunier                     */
 /*                                                                      */
+/* api NPDS proto 01                                                    */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
@@ -14,25 +15,18 @@ if (!function_exists("Mysql_Connexion")) {
    include ("mainfile.php");
 }
 
-
 function stripslashes_deep($value) {
-    $value = is_array($value) ?
-                array_map('stripslashes_deep', $value) :
-                stripslashes($value);
-    return $value;
+   $value = is_array($value) ?
+   array_map('stripslashes_deep', $value) :
+   stripslashes($value);
+   return $value;
 }
-
-
-
-
-
-
 
 function api_getusers() {
 global $una;
-if ($una=='Tous') {$where='';} else{$where=" where uname='".$una."'"; };
+if ($una=='Tous') {$where='';} else{$where=" WHERE uname='".$una."'"; };
 settype($ap_users,'array');
-$res=sql_query("select uid, uname, name, user_avatar, user_regdate from ".$NPDS_Prefix."users ".$where);
+$res=sql_query("SELECT uid, uname, name, user_avatar, user_regdate FROM ".$NPDS_Prefix."users ".$where);
 while($ap_user = sql_fetch_assoc($res)){
 $ap_users[]=$ap_user;
 }
@@ -44,7 +38,7 @@ function api_getgroups() {
 global $gna;
 if ($gna=='Tous') {$where='';} else{$where=" where groupe_name='".$gna."'"; };
 settype($ap_groups,'array');
-$res=sql_query("select groupe_id, groupe_name, groupe_description from ".$NPDS_Prefix."groupes ".$where);
+$res=sql_query("SELECT groupe_id, groupe_name, groupe_description FROM ".$NPDS_Prefix."groupes ".$where);
 while($ap_group = sql_fetch_assoc($res)){
 $ap_groups[]=$ap_group;
 }
@@ -55,16 +49,16 @@ print json_encode( $ap_groups );
 
 function api_getusers_post() {
 global $upa;
-$res=sql_query("select count(*) as total from ".$NPDS_Prefix."forumtopics where topic_poster = $upa ");
+$res=sql_query("SELECT count(*) as total FROM ".$NPDS_Prefix."forumtopics WHERE topic_poster = $upa ");
 $ap_user_post = sql_fetch_assoc($res);
 
-$res_1 =sql_query("select * from ".$NPDS_Prefix."forumtopics where topic_poster = $upa ");
+$res_1 =sql_query("SELECT * FROM ".$NPDS_Prefix."forumtopics WHERE topic_poster = $upa ");
 while($ap_top = sql_fetch_assoc($res_1)){
 $ap_tops[]=$ap_top;
 $ap_fo=$ap_top['topic_id'];
 
 $res_2= sql_query("select * from forumtopics ft
-LEFT JOIN posts ON ft.topic_id = posts.topic_id LEFT JOIN forums ON ft.forum_id = forums.forum_id where ft.topic_id=$ap_fo group by ft.topic_id
+LEFT JOIN posts ON ft.topic_id = posts.topic_id LEFT JOIN forums ON ft.forum_id = forums.forum_id WHERE ft.topic_id=$ap_fo GROUP BY ft.topic_id
 ");
 while($ap_for = sql_fetch_assoc($res_2)){
 $ap_fors[]=$ap_for;
@@ -73,15 +67,9 @@ $ap_fors[]=$ap_for;
 
 }
 
-
-
-
-
-
 print '[ {"user_post":'.json_encode( $ap_user_post['total'] ).'}'.',{"user_top":'.json_encode( $ap_tops).'},{"topic_detail":'.json_encode($ap_fors).'} ]';
 
 }
-
 
 function api_getdownload() {
 // data (*) de download categorie x ou tout
@@ -95,7 +83,6 @@ $ap_dows[]=$ap_dow;
 $ap_dows=stripslashes_deep($ap_dows);
 print json_encode( $ap_dows );
 }
-
 
 switch ($op) {
    case "api_getusers":
@@ -111,7 +98,5 @@ switch ($op) {
         break;
 }
 // etc etc...
-
-
 
 ?>
