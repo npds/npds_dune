22aeb0044e9d1528784d12b3375302d99f0a4746
jpb <jean.barbary@wanadoo.fr>
2016-02-10 00:06:11 +0100

    mep

--- revolution_16/modules/ipban/setban.php
+++ revolution_16/modules/ipban/setban.php
@@ -3,7 +3,7 @@
 /* DUNE by NPDS                                                         */
 /* ===========================                                          */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2014 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2015 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -15,11 +15,11 @@ $f_meta_nom ='setban';
 //==> controle droit
 admindroits($aid,$f_meta_nom);
 //<== controle droit
-include ("modules/$ModPath/ipban.lang-$language.php");
+include ("modules/$ModPath/lang/ipban.lang-$language.php");
 $f_titre = ipban_translate("Administration de l'IpBan");
 
 function ConfigureBan($ModPath, $ModStart) {
-global $f_meta_nom, $f_titre, $adminimg;
+global $f_meta_nom, $f_titre, $adminimg,$language;
    if (file_exists("slogs/spam.log")) {
       $fd = fopen ("slogs/spam.log", "r");
       while (!feof($fd)) {
@@ -28,41 +28,45 @@ global $f_meta_nom, $f_titre, $adminimg;
       fclose ($fd);
    }
 adminhead ($f_meta_nom, $f_titre, $adminimg);
-
 echo '
-   <div>
-      <p>'.ipban_translate("Chaque ligne ne doit contenir qu'une adresse IP de la forme : a.b.c.d:<b>X</b> (ex : 168.192.1.1:5)<br />si <b>X</b> >= 5 alors l'accès sera refusé<br /><br />Ce fichier est mis à jour automatiquement par l'anti-spam de NPDS.").'</p>
+   <hr />
+      <div class="card card-block">
+         '.ipban_translate("Chaque ligne ne doit contenir qu'une adresse IP de la forme : a.b.c.d:X (ex : 168.192.1.1:5).").'<br />
+         <span class="text-danger">'.ipban_translate("Si X >= 5 alors l'accÃ¨s sera refusÃ© !").'</span><br />
+         '.ipban_translate("Ce fichier est mis Ã  jour automatiquement par l'anti-spam de NPDS.").'
+      </div>
       <form id="ipban_mod" action="admin.php" method="post">
-         <div class="form-group">
-            <label class="control-label" for="ip_ban">'.ipban_translate("Liste des IP").'</label>
-            <textarea id="ip_ban" class="form-control" name="ipban" rows="20">'.$ip_ban.'</textarea>
+         <div class="form-group row">
+            <label class="form-control-label col-sm-12" for="ip_ban">'.ipban_translate("Liste des IP").'</label>
+            <div class="col-sm-12">
+               <textarea id="ip_ban" class="form-control" name="ipban" rows="15">'.$ip_ban.'</textarea>
+            </div>
          </div>
-         <div class="form-group">
-            <button class="btn btn-primary" type="submit">'.adm_translate("Sauver les modifications").'</button>
-            <input type="hidden" name="op" value="Extend-Admin-SubModule" />
-            <input type="hidden" name="ModPath" value="'.$ModPath.'" />
-            <input type="hidden" name="ModStart" value="'.$ModStart.'" />
-            <input type="hidden" name="subop" value="SaveSetBan" />
+         <div class="form-group row">
+            <div class="col-sm-12">
+               <button class="btn btn-primary" type="submit">'.adm_translate("Sauver les modifications").'</button>
+               <input type="hidden" name="op" value="Extend-Admin-SubModule" />
+               <input type="hidden" name="ModPath" value="'.$ModPath.'" />
+               <input type="hidden" name="ModStart" value="'.$ModStart.'" />
+               <input type="hidden" name="subop" value="SaveSetBan" />
+            </div>
          </div>
-      </form>
-   </div>
-</div>';
+      </form>';
+   adminfoot('','','','');
 }
 
 function SaveSetBan($Xip_ban) {
    $file = fopen("slogs/spam.log", "w");
       fwrite($file, $Xip_ban);
    fclose($file);
-
    SC_clean();
 }
 
 switch ($subop) {
-    case "SaveSetBan":
-    SaveSetBan($ipban);
-
-    default:
-    ConfigureBan($ModPath, $ModStart);
-    break;
+   case 'SaveSetBan':
+   SaveSetBan($ipban);
+   default:
+   ConfigureBan($ModPath, $ModStart);
+   break;
 }
 ?>
