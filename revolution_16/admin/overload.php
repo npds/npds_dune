daeaf30be2f83abcda6b759399f07344149b9f98
jpb <jean.barbary@wanadoo.fr>
2016-02-10 00:08:11 +0100

    mep+inside code

--- revolution_16/admin/overload.php
+++ revolution_16/admin/overload.php
@@ -75,9 +75,9 @@ function main() {
    GraphicAdmin($hlpfile);
    adminhead ($f_meta_nom, $f_titre, $adminimg);
 
-//   if ($radminsuper==1) {
-      include("cache.timings.php");
-      echo '
+   include("cache.timings.php");
+   echo '
+      <hr />
       <form id="fad_cache" action="admin.php" method="post">
       <fieldset>
       <legend>'.adm_translate("Activation").'</legend>
@@ -105,97 +105,91 @@ function main() {
          </div>
       </fieldset>';
 
-      if (($CACHE_TIMINGS['index.php']=="") or (!isset($CACHE_TIMINGS['index.php'])))             {$CACHE_TIMINGS['index.php']=300;}
-      if (($CACHE_TIMINGS['article.php']=="") or (!isset($CACHE_TIMINGS['article.php'])))         {$CACHE_TIMINGS['article.php']=60;}
-      if (($CACHE_TIMINGS['sections.php']=="") or (!isset($CACHE_TIMINGS['sections.php'])))       {$CACHE_TIMINGS['sections.php']=300;}
-      if (($CACHE_TIMINGS['faq.php']=="") or (!isset($CACHE_TIMINGS['faq.php'])))                 {$CACHE_TIMINGS['faq.php']=86400;}
-      if (($CACHE_TIMINGS['links.php']=="") or (!isset($CACHE_TIMINGS['links.php'])))             {$CACHE_TIMINGS['links.php']=28800;}
-      if (($CACHE_TIMINGS['forum.php']=="") or (!isset($CACHE_TIMINGS['forum.php'])))             {$CACHE_TIMINGS['forum.php']=3600;}
-      if (($CACHE_TIMINGS['memberslist.php']=="") or (!isset($CACHE_TIMINGS['memberslist.php']))) {$CACHE_TIMINGS['memberslist.php']=1800;}
-      if (($CACHE_TIMINGS['modules.php']=="") or (!isset($CACHE_TIMINGS['modules.php'])))         {$CACHE_TIMINGS['modules.php']=3600;}
+      if (($CACHE_TIMINGS['index.php']=='') or (!isset($CACHE_TIMINGS['index.php'])))             {$CACHE_TIMINGS['index.php']=300;}
+      if (($CACHE_TIMINGS['article.php']=='') or (!isset($CACHE_TIMINGS['article.php'])))         {$CACHE_TIMINGS['article.php']=60;}
+      if (($CACHE_TIMINGS['sections.php']=='') or (!isset($CACHE_TIMINGS['sections.php'])))       {$CACHE_TIMINGS['sections.php']=300;}
+      if (($CACHE_TIMINGS['faq.php']=='') or (!isset($CACHE_TIMINGS['faq.php'])))                 {$CACHE_TIMINGS['faq.php']=86400;}
+      if (($CACHE_TIMINGS['links.php']=='') or (!isset($CACHE_TIMINGS['links.php'])))             {$CACHE_TIMINGS['links.php']=28800;}
+      if (($CACHE_TIMINGS['forum.php']=='') or (!isset($CACHE_TIMINGS['forum.php'])))             {$CACHE_TIMINGS['forum.php']=3600;}
+      if (($CACHE_TIMINGS['memberslist.php']=='') or (!isset($CACHE_TIMINGS['memberslist.php']))) {$CACHE_TIMINGS['memberslist.php']=1800;}
+      if (($CACHE_TIMINGS['modules.php']=='') or (!isset($CACHE_TIMINGS['modules.php'])))         {$CACHE_TIMINGS['modules.php']=3600;}
 
       echo '
       <fieldset>
-      <legend>'.adm_translate("Temps de Rétention en secondes").'</legend>
+      <legend>'.adm_translate("Temps de rétention en secondes").'</legend>
       <div class="form-group">
         <div class="row">
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_index"><code>index.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_index" name="xt_index" value="'.$CACHE_TIMINGS['index.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 300</span>
             </div>
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_article"><code>article.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_article" name="xt_article" value="'.$CACHE_TIMINGS['article.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 300</span>
             </div>
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_sections"><code>sections.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_sections" name="xt_sections" value="'.$CACHE_TIMINGS['sections.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 300</span>
             </div>
             </div>
          <div class="row">
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_faq"><code>faq.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_faq" name="xt_faq" value="'.$CACHE_TIMINGS['faq.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 86400</span>
             </div>
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_links"><code>links.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_links" name="xt_links" value="'.$CACHE_TIMINGS['links.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 28800</span>
             </div>
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_forum"><code>forum.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_forum" name="xt_forum" value="'.$CACHE_TIMINGS['forum.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 3600</span>
             </div>
             </div>
          <div class="row">
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_memberlist"><code>memberlist.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_memberlist" name="xt_memberlist" value="'.$CACHE_TIMINGS['memberslist.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 1800</span>
             </div>
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class=" col-sm-4 col-xs-12">
                <label for="xt_modules"><code>modules.php</code></label>
                <input class="form-control" type="number" min="0" id="xt_modules" name="xt_modules" value="'.$CACHE_TIMINGS['modules.php'].'" required="required" data-fv-row=".col-md-4" /><span class="help-block">Def : 3600</span>
             </div>
          </div>
       </div>
       </fieldset>
-      <div class="form-group">
-         <div class="row">
-            <div class="col-xs-12">
-               <button class="btn btn-primary btn-block" type="submit"><i class="fa fa-check-square fa-lg">&nbsp;</i>'.adm_translate("Valider").'</button>
-            </div>
+      <div class="form-group row">
+         <input type="hidden" name="op" value="supercache_save" />
+         <div class="col-xs-12">
+            <button class="btn btn-primary-outline col-xs-12 text-xs-left" type="submit"><i class="fa fa-check fa-lg">&nbsp;</i>'.adm_translate("Valider").'</button>
          </div>
       </div>
-      <input type="hidden" name="op" value="supercache_save" />
       </form>
+      <hr />
       <form id="fad_cacheclean" action="admin.php" method="post">
-         <div class="form-group">
-            <div class="row">
-               <div class="col-xs-12">
-                  <button class="btn btn-danger btn-block" type="submit"><i class="fa fa-trash-o fa-lg">&nbsp;</i>'.adm_translate("Vider le répertoire cache").'</button>
-               </div>
+         <div class="form-group row">
+            <div class="col-xs-12">
+               <button class="btn btn-danger-outline col-xs-12 pull-xs-right text-xs-right" type="submit">'.adm_translate("Vider le répertoire cache").'&nbsp;<i class="fa fa-trash-o fa-lg"></i></button>
             </div>
          </div>
          <input type="hidden" name="op" value="supercache_empty" />
       </form>';
-//   }
    adminfoot('fv','','','');
 }
 
 switch ($op) {
-   case "supercache_save":
+   case 'supercache_save':
       if ($radminsuper==1) {
         save_supercache($xsupercache, $xt_index, $xt_article, $xt_sections, $xt_faq, $xt_links, $xt_forum, $xt_memberlist, $xt_modules);
       }
-      global $aid; Ecr_Log("security", "ChangeSuperCache($xsupercache, $xt_index, $xt_article, $xt_sections, $xt_faq, $xt_links, $xt_forum, $xt_memberlist, $xt_modules) by AID : $aid", "");
+      global $aid; Ecr_Log('security', "ChangeSuperCache($xsupercache, $xt_index, $xt_article, $xt_sections, $xt_faq, $xt_links, $xt_forum, $xt_memberlist, $xt_modules) by AID : $aid", '');
       Header("Location: admin.php?op=supercache");
       break;
-
-   case "supercache_empty":
+   case 'supercache_empty':
       SC_clean();
-      global $aid; Ecr_Log("security", "EmptySuperCache() by AID : $aid", "");
+      global $aid; Ecr_Log('security', "EmptySuperCache() by AID : $aid", '');
       Header("Location: admin.php?op=supercache");
       break;
-
    default:
       main();
       break;
