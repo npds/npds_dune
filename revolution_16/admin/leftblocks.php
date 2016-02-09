2159b352a2bff3ef10f08064983efc266c437b11
jpb <jean.barbary@wanadoo.fr>
2016-02-10 00:01:44 +0100

    inside code

--- revolution_16/admin/leftblocks.php
+++ revolution_16/admin/leftblocks.php
@@ -12,8 +12,7 @@
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 if (!stristr($_SERVER['PHP_SELF'],"admin.php")) { Access_Error(); }
-$f_meta_nom ='blocks';// a voir si on réimplémente les droits spécifique droit et gauche
-$f_titre = adm_translate("Grands Titres de sites de News");
+$f_meta_nom ='blocks';// à voir si on réimplémente les droits spécifique droit et gauche
 //==> controle droit
 admindroits($aid,$f_meta_nom);
 //<== controle droit
@@ -25,17 +24,17 @@ function makelblock($title, $content, $members, $Mmember, $Lindex, $Scache, $BLa
    global $NPDS_Prefix;
 
    if (is_array($Mmember) and ($members==1)) {
-      $members=implode(",",$Mmember);
+      $members=implode(',',$Mmember);
       if ($members==0) $members=1;
    }
    if (empty($Lindex)) {$Lindex=0;}
    $title=stripslashes(FixQuotes($title));
    $content = stripslashes(FixQuotes($content));
-   if ($SHTML!="ON")
-      $content = strip_tags(str_replace("<br />","\n",$content));
-   sql_query("insert INTO ".$NPDS_Prefix."lblocks VALUES (NULL,'$title','$content','$members', '$Lindex', '$Scache', '1','$css', '$BLaide')");
+   if ($SHTML!='ON')
+      $content = strip_tags(str_replace('<br />','\n',$content));
+   sql_query("INSERT INTO ".$NPDS_Prefix."lblocks VALUES (NULL,'$title','$content','$members', '$Lindex', '$Scache', '1','$css', '$BLaide')");
 
-   global $aid; Ecr_Log("security", "MakeLeftBlock($title) by AID : $aid", "");
+   global $aid; Ecr_Log('security', "MakeLeftBlock($title) by AID : $aid", "");
    Header("Location: admin.php?op=blocks");
 }
 
@@ -43,12 +42,12 @@ function changelblock($id, $title, $content, $members, $Mmember, $Lindex, $Scach
    global $NPDS_Prefix;
 
    if (is_array($Mmember) and ($members==1)) {
-      $members=implode(",",$Mmember);
+      $members=implode(',',$Mmember);
       if ($members==0) $members=1;
    }
    if (empty($Lindex)) {$Lindex=0;}
    $title=stripslashes(FixQuotes($title));
-   if ($Sactif=="ON") {$Sactif=1;} else {$Sactif=0;}
+   if ($Sactif=='ON') {$Sactif=1;} else {$Sactif=0;}
    if ($css){ $css=1;}else{$css=0;}
    $content = stripslashes(FixQuotes($content));
    $BLaide = stripslashes(FixQuotes($BLaide));
@@ -62,40 +61,40 @@ function changedroitelblock($id, $title, $content, $members, $Mmember, $Lindex, 
    global $NPDS_Prefix;
 
    if (is_array($Mmember) and ($members==1)) {
-      $members=implode(",",$Mmember);
+      $members=implode(',',$Mmember);
       if ($members==0) $members=1;
    }
    if (empty($Lindex)) {$Lindex=0;}
    $title=stripslashes(FixQuotes($title));
-   if ($Sactif=="ON") {$Sactif=1;} else {$Sactif=0;}
+   if ($Sactif=='ON') {$Sactif=1;} else {$Sactif=0;}
    if ($css){ $css=1;}else{$css=0;}
    $content = stripslashes(FixQuotes($content));
    $BLaide = stripslashes(FixQuotes($BLaide));
    sql_query("INSERT INTO ".$NPDS_Prefix."rblocks VALUES (NULL,'$title','$content', '$members', '$Lindex', '$Scache', '$Sactif', '$css', '$BLaide')");
    sql_query("DELETE FROM ".$NPDS_Prefix."lblocks WHERE id='$id'");
 
-   global $aid; Ecr_Log("security", "MoveLeftBlockToRight($title - $id) by AID : $aid", "");
+   global $aid; Ecr_Log('security', "MoveLeftBlockToRight($title - $id) by AID : $aid", '');
    Header("Location: admin.php?op=blocks");
 }
 
 function deletelblock($id) {
    global $NPDS_Prefix;
    sql_query("DELETE FROM ".$NPDS_Prefix."lblocks WHERE id='$id'");
-   global $aid; Ecr_Log("security", "DeleteLeftBlock($id) by AID : $aid", "");
+   global $aid; Ecr_Log('security', "DeleteLeftBlock($id) by AID : $aid", '');
    Header("Location: admin.php?op=blocks");
 }
 
 switch ($op) {
-   case "makelblock":
+   case 'makelblock':
         makelblock($title, $xtext, $members, $Mmember, $index, $Scache, $Baide, $SHTML, $css);
         break;
-   case "deletelblock":
+   case 'deletelblock':
         deletelblock($id);
         break;
-   case "changelblock":
+   case 'changelblock':
         changelblock($id, $title, $content, $members, $Mmember, $Lindex, $Scache, $Sactif, $BLaide, $css);
         break;
-   case "droitelblock":
+   case 'droitelblock':
         changedroitelblock($id, $title, $content, $members, $Mmember, $Lindex, $Scache, $Sactif, $BLaide, $css);
         break;
 }
