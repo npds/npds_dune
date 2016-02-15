77b81f14f62eba73d87e40fee77329ff3efa6fa1
jpb <jean.barbary@wanadoo.fr>
2016-02-15 15:11:05 +0100

    date

--- revolution_16/sections.config.php
+++ revolution_16/sections.config.php
@@ -2,7 +2,7 @@
 # # DUNE by NPDS : Net Portal Dynamic System
 # ===================================================
 #
-# This version name NPDS Copyright (c) 2001-2012 by Philippe Brunier
+# This version name NPDS Copyright (c) 2001-2015 by Philippe Brunier
 #
 # This module is to configure the Sections 
 #
