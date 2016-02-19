70dcd37179d59cafcf47be71ffef92cc712ce405
jpb <jean.barbary@wanadoo.fr>
2016-02-19 22:06:36 +0100

    clean code

--- revolution_16/modules/reseaux-sociaux/reseaux-sociaux.php
+++ revolution_16/modules/reseaux-sociaux/reseaux-sociaux.php
@@ -15,30 +15,35 @@
 /* version 1.0 17/02/2016                                               */
 /************************************************************************/
 
-
 if (strstr($ModPath,"..") || strstr($ModStart,"..") || stristr($ModPath, "script") || stristr($ModPath, "cookie") || stristr($ModPath, "iframe") || stristr($ModPath, "applet") || stristr($ModPath, "object") || stristr($ModPath, "meta") || stristr($ModStart, "script") || stristr($ModStart, "cookie") || stristr($ModStart, "iframe") || stristr($ModStart, "applet") || stristr($ModStart, "object") || stristr($ModStart, "meta")) {
    die();
 }
 if (!function_exists("Mysql_Connexion")) {
    include ("mainfile.php");
 }
-$ModStart='reseaux-sociaux';
+if (!$user) header('location:index.php');
 
-function ListReseaux($ModPath, $ModStart, $f_meta_nom, $f_titre, $adminimg) {
+global $language;
+$ModStart='reseaux-sociaux';
+include ("modules/$ModPath/lang/rs-$language.php");
+
+function ListReseaux($ModPath, $ModStart) {
    if (file_exists("modules/$ModPath/reseaux-sociaux.conf.php"))
       include ("modules/$ModPath/reseaux-sociaux.conf.php");
    include("header.php");
 
    echo '
-   <h3>'.translate("Social networks").'</h3>
+   <h3>'.rs_translate("Réseaux sociaux").'</h3>
    <hr />
-   <h3><a href="modules.php?&amp;ModPath='.$ModPath.'&amp;ModStart='.$ModStart.'&amp;op=EditReseaux"><i class="fa fa-edit fa-lg"></i></a>&nbsp;'.translate("Edit").'</h3>
+   <h3><a href="modules.php?&amp;ModPath='.$ModPath.'&amp;ModStart='.$ModStart.'&amp;op=EditReseaux"><i class="fa fa-edit fa-lg"></i></a>&nbsp;'.rs_translate("Editer").'</h3>
+'.rs_translate("Liste des réseaux sociaux mis à disposition par l'administrateur.").'
+
    <table id ="lst_res_soc" data-toggle="table" data-striped="true" data-search="true" data-show-toggle="true" data-mobile-responsive="true" data-icons-prefix="fa" data-icons="icons">
       <thead>
          <tr>
-            <th data-sortable="true" data-halign="center" data-align="right">'.translate("Name").'</th>
-            <th data-halign="center" data-align="center">'.translate("Icon").'</th>
-            <th data-halign="center" data-align="right">'.translate("Functions").'</th>
+            <th data-sortable="true" data-halign="center" data-align="right">'.rs_translate("Name").'</th>
+            <th data-halign="center" data-align="center">'.rs_translate("Icône").'</th>
+            <th data-halign="center" data-align="right">'.rs_translate("Fonctions").'</th>
          </tr>
       </thead>
       <tbody>';
@@ -49,18 +54,17 @@ function ListReseaux($ModPath, $ModStart, $f_meta_nom, $f_titre, $adminimg) {
             <td>'.$v1[0].'</td>
             <td><i class="fa fa-'.$v1[2].' fa-2x text-primary"></i></td>
             <td>
-               <a href="modules.php?&amp;ModPath='.$ModPath.'&amp;ModStart='.$ModStart.'&amp;op=EditReseaux&amp;rs_id='.urlencode($v1[0]).'" ><i class="fa fa-edit fa-lg" title="'.translate("Edit").'" data-toggle="tooltip"></i></a>
-               <a href="modules.php?&amp;ModPath='.$ModPath.'&amp;ModStart='.$ModStart.'&amp;op=DeleteReseaux&amp;rs_id='.urlencode($v1[0]).'" ><i class="fa fa-trash-o fa-lg text-danger" title="'.translate("Delete").'" data-toggle="tooltip"></i></a>
+               <a href="modules.php?&amp;ModPath='.$ModPath.'&amp;ModStart='.$ModStart.'&amp;op=EditReseaux&amp;rs_id='.urlencode($v1[0]).'" ><i class="fa fa-edit fa-lg" title="'.rs_translate("Editer").'" data-toggle="tooltip"></i></a>
             </td>
         </tr>';
    }
    echo '
       </tbody>
    </table>';
-   adminfoot('fv','','','');
+   adminfoot('','','','');
 }
 
-function EditReseaux($ModPath, $ModStart, $rs_id, $rs_uid, $op) {
+function EditReseaux($ModPath, $ModStart, $rs_id, $rs_uid) {
    if (file_exists("modules/$ModPath/reseaux-sociaux.conf.php"))
       include ("modules/$ModPath/reseaux-sociaux.conf.php");
    include("header.php");
@@ -90,7 +94,7 @@ function EditReseaux($ModPath, $ModStart, $rs_id, $rs_uid, $op) {
 */
 
 
-   echo '<h3>'.translate("Social networks").'</h3>';
+   echo '<h3>'.rs_translate("Réseaux sociaux").'</h3>';
    echo'
    <hr />
    <form id="reseaux_user" action="modules.php?ModStart='.$ModStart.'&amp;ModPath='.$ModPath.'&amp;op=SaveSetReseaux" method="post">';
@@ -111,23 +115,25 @@ function EditReseaux($ModPath, $ModStart, $rs_id, $rs_uid, $op) {
    <fieldset>
    <legend><i class="fa fa-'.$v1[2].' fa-2x text-primary">&nbsp;</i>'.$v1[0].'</legend>
       <div class="form-group row">
-         <label class="form-control-label col-sm-12" for="rs_uid'.$i.'">'.translate("Nickname").'</label>
+         <label class="form-control-label col-sm-12" for="rs_uid'.$i.'">'.rs_translate("Identifiant").'</label>
          <div class="col-sm-12">
-            <input id="rs_uid'.$i.'" class="form-control" type="text" name="rs['.$i.'][uid]"  maxlength="50"  placeholder="'.translate("Nickname").' '.$v1[0].'" value="'.$ident.'"/>
+            <input id="rs_uid'.$i.'" class="form-control" type="text" name="rs['.$i.'][uid]"  maxlength="50"  placeholder="'.rs_translate("Identifiant").' '.$v1[0].'" value="'.$ident.'"/>
             <span class="help-block text-xs-right"><span id="countcar_rs_uid'.$i.'"></span></span>
             <input type="hidden" name="rs['.$i.'][id]" value="'.$v1[0].'" />
          </div>
       </div>
    </fieldset>
    </div>';
-   if ($nombre%2 == 1) echo '</div><div class="row">';
+   if ($nombre%2 == 1) echo '
+   </div>
+   <div class="row">';
    $i++;
    }
 echo'
    </div>
       <div class="form-group row">
          <div class="col-sm-offset-6 col-sm-6">
-            <button class="btn btn-primary col-xs-12" type="submit"><i class="fa fa-check fa-lg"></i>&nbsp;'.translate("Save").'</button>
+            <button class="btn btn-primary col-xs-12" type="submit"><i class="fa fa-check fa-lg"></i>&nbsp;'.rs_translate("Sauvegarder").'</button>
             <input type="hidden" name="ModPath" value="'.$ModPath.'" />
             <input type="hidden" name="ModStart" value="'.$ModStart.'" />
             <input type="hidden" name="op" value="SaveSetReseaux" />
@@ -153,37 +159,15 @@ function SaveSetReseaux($ModPath, $ModStart) {
    sql_query("UPDATE ".$NPDS_Prefix."users_extend SET M2='$li_rs' WHERE uid='$cookie[0]'");
    Header("Location: modules.php?&ModPath=$ModPath&ModStart=$ModStart");
 
-   
-   
-   
 }
 
    switch ($op) {
        case "SaveSetReseaux":
        SaveSetReseaux($ModPath, $ModStart);break;
-       case "DeleteReseaux":
-       SaveSetReseaux($ModPath, $ModStart);break;
-       case "AddReseaux":
-       EditReseaux($ModPath, $ModStart, $rs_id, $rs_uid);break;
        case "EditReseaux":
-       EditReseaux($ModPath, $ModStart, $f_meta_nom, $f_titre, $adminimg, $rs_id, $rs_url, $rs_ico, $subop);break;
+       EditReseaux($ModPath, $ModStart, $rs_id, $rs_url, $rs_ico, $subop);break;
        default:
-       ListReseaux($ModPath, $ModStart,$f_meta_nom, $f_titre, $adminimg);
+       ListReseaux($ModPath, $ModStart);
     break;
    }
-
-
-
-
-
-
-
-
-   
-   
-   
-   
-   
-   
-   
 ?>
