f6796f6325b161126c36903da4423552d1bf49da
jpb <jean.barbary@wanadoo.fr>
2016-02-20 18:44:24 +0100

    comment

--- revolution_16/install_new/sql/build_sql-create.php
+++ revolution_16/install_new/sql/build_sql-create.php
@@ -10,12 +10,14 @@
 /*         : v.1.1.1 jpb, phr                                           */
 /*         : v.1.1.2 jpb, phr, dev, boris                               */
 /*         : v.1.1.3 dev - 2013                                         */
-/*         : v.1.2 phr, jpb - 2016                                      */
+/*         : v.1.2 0 phr, jpb - 2016                                    */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
+
+// ATTENTION ce script ne fonctionne parfaitement qu'avec une archive SQL avec des instructions SQL répétitives individualisées  !!
 
 function build_sql_create($NPDS_Prefix) {
 
@@ -66,13 +68,13 @@ $content .= "/* DUNE by NPDS                                                    
 $content .= "/* ===========================                                          */\n";
 $content .= "/*                                                                      */\n";
 $content .= "/* NPDS Copyright (c) 2002-2016 by Philippe Brunier                     */\n";
-$content .= "/* IZ-Xinstall version : 1.1.3                                          */\n";
+$content .= "/* IZ-Xinstall version : 1.2.0                                          */\n";
 $content .= "/*                                                                      */\n";
 $content .= "/* Auteurs : v.0.1.0 EBH (plan.net@free.fr)                             */\n";
 $content .= "/*         : v.1.1.1 jpb, phr                                           */\n";
 $content .= "/*         : v.1.1.2 jpb, phr, dev, boris                               */\n";
 $content .= "/*         : v.1.1.3 dev - 2013                                         */\n";
-$content .= "/*         : v.1.2 phr, jpb - 2016                                      */\n";
+$content .= "/*         : v.1.2.0 phr, jpb - 2016                                    */\n";
 $content .= "/*                                                                      */\n";
 $content .= "/* This program is free software. You can redistribute it and/or modify */\n";
 $content .= "/* it under the terms of the GNU General Public License as published by */\n";
