32376973fe980bf80b7302b78f01935b236a8537
jpb <jean.barbary@wanadoo.fr>
2016-03-02 23:07:43 +0100

    mep + inside code

--- revolution_16/modules/comments/admin.php
+++ revolution_16/modules/comments/admin.php
@@ -6,7 +6,7 @@
 /* Based on PhpNuke 4.x source code                                     */
 /* Based on Parts of phpBB                                              */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2010 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2015 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -30,7 +30,7 @@ if ($forum>=0)
    die();
 
 // gestion des params du 'forum' : type, accès, modérateur ...
-$forum_name = "comments";
+$forum_name = 'comments';
 $forum_type=0;
 $allow_to_post=false;
 if ($anonpost)
@@ -38,12 +38,11 @@ if ($anonpost)
 else
    $forum_access=1;
 
-
 if (($moderate==1) and $admin)
    $Mmod=true;
 elseif ($moderate==2) {
    $userX=base64_decode($user);
-   $userdata=explode(":", $userX);
+   $userdata=explode(':', $userX);
    $result=sql_query("SELECT level FROM ".$NPDS_Prefix."users_status WHERE uid='".$userdata[0]."'");
    list($level)=sql_fetch_row($result);
    if ($level>=2)
@@ -59,34 +58,25 @@ elseif ($moderate==2) {
                if (!$result = sql_query($sql))
                   forumerror('0009');
                // ordre de mise à jour d'un champ externe ?
-               if ($comments_req_raz!="")
+               if ($comments_req_raz!='')
                   sql_query("UPDATE ".$NPDS_Prefix.$comments_req_raz);
                redirect_url("$url_ret");
                break;
             case 'viewip':
                include("header.php");
-               opentable();
                $sql = "SELECT u.uname, p.poster_ip, p.poster_dns FROM ".$NPDS_Prefix."users u, posts p WHERE p.post_id = '$post' AND u.uid = p.poster_id";
                if (!$r = sql_query($sql))
                   forumerror('0013');
                if (!$m = sql_fetch_assoc($r))
                   forumerror('0014');
-               echo "<table border=\"0\" cellpadding=\"1\" cellspacing=\"1\" width=\"100%\">
-                     <tr>
-                     <td class=\"header\" colspan=\"2\">".translate("Users IP and Account information")."</td>
-                     </tr><tr>
-                     <td>".translate("Nickname: ")."</td>
-                     <td>".$m['uname']."</td>
-                     </tr><tr>
-                     <td>".translate("User IP: ")."</td>
-                     <td>".$m['poster_ip']."</td>
-                     </tr><tr>
-                     <td>".translate("User DNS: ")."</td>
-                     <td>".$m['poster_dns']."</td>
-                     </tr>
-                     </table>";
-               echo "<br /><p align=\"center\"><a href=\"".rawurldecode($url_ret)."\" class=\"noir\">".translate("Go Back")."</a></p>";
-               closetable();
+               echo '
+      <h3>'.translate("Users IP and Account information").'</h3>
+      <div class="card card-block">
+         <strong>'.translate("Nickname: ").'</strong> '.$m['uname'].'<br />
+         <strong>'.translate("User IP: ").'</strong> '.$m['poster_ip'].'<br />
+         <strong>'.translate("User DNS: ").'</strong> '.$m['poster_dns'].'<br />
+      </div>
+      <p><a href="'.rawurldecode($url_ret).'" class="btn btn-primary">'.translate("Go Back").'</a></p>';
                include("footer.php");
                break;
             case 'aff':
@@ -95,10 +85,10 @@ elseif ($moderate==2) {
 
                // ordre de mise à jour d'un champ externe ?
                if ($ordre) {
-                  if ($comments_req_add!="")
+                  if ($comments_req_add!='')
                      sql_query("UPDATE ".$NPDS_Prefix.$comments_req_add);
                } else {
-                  if ($comments_req_del!="")
+                  if ($comments_req_del!='')
                      sql_query("UPDATE ".$NPDS_Prefix.$comments_req_del);
                }
                redirect_url("$url_ret");
