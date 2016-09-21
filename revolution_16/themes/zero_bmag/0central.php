88a85f96520136dc3f56b1f4edb95b6f04423c2b
jpb <jean.barbary@wanadoo.fr>
2016-09-21 10:09:00 +0200

    theme zero work

--- revolution_16/themes/zero_bmag/0central.php
+++ revolution_16/themes/zero_bmag/0central.php
@@ -1,6 +1,6 @@
 <?php
 /************************************************************** **********/
-/* Modification par Jireck      Rocssti2015                                          */
+/* Modification par Jireck      zero_bmag                                          */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
