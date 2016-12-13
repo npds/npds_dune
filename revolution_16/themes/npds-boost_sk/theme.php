c72871aa4f52982f3a065a79839c9b7149bd4c9f
jpb <jean.barbary@wanadoo.fr>
2016-12-13 18:50:03 +0100

    npds-boost_sk theme work

--- revolution_16/themes/npds-boost_sk/theme.php
+++ revolution_16/themes/npds-boost_sk/theme.php
@@ -4,13 +4,13 @@
 /*======================================================================*/
 /* This theme use the NPDS theme-dynamic engine (Meta-Lang)             */
 /*                                                                      */
-/* Theme : npds-boost 2015 by jpb                                       */
+/* Theme : npds-boost_sk 2015 by jpb                                    */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
-$theme      = 'npds-boost_sk';
+$theme = 'npds-boost_sk';
 $long_chain = '34'; // Nombre de caractères affichés avant troncature pour certains blocs
 
 // ne pas supprimer cette ligne / Don't remove this line
