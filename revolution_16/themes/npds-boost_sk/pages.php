d59c37473bd91e72ea80d2a1d863a8f244197a6a
jpb <jean.barbary@wanadoo.fr>
2016-12-24 11:53:16 +0100

    no need

---
+++ revolution_16/themes/npds-boost_sk/pages.php
@@ -0,0 +1,14 @@
+<?php
+/************************************************************************/
+/* Theme for NPDS / Net Portal Dynamic System                           */
+/* =====================================================================*/
+/*                                                                      */
+/* Theme : "npds-boost" version 2015 par jpb/phr                        */
+/* This theme use the NPDS theme-dynamic engine (DynaMot)               */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+?>
