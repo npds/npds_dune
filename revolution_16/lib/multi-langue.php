2a5fdfb620780c4d264f797430700114238dd388
jpb <jean.barbary@wanadoo.fr>
2017-01-02 13:19:50 +0100

    (c) -2017 update

--- revolution_16/lib/multi-langue.php
+++ revolution_16/lib/multi-langue.php
@@ -3,7 +3,7 @@
 /* NPDS V : Net Portal Dynamic System .                                 */
 /* ===========================                                          */
 /*                                                                      */
-/* This version name NPDS Copyright (c) 2001-2004 by Philippe Brunier   */
+/* This version name NPDS Copyright (c) 2001-2017 by Philippe Brunier   */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -11,28 +11,28 @@
 /************************************************************************/
 
 // Multi-language
-$local_path="";
-if (isset($module_mark)) {$local_path="../../";}
+$local_path='';
+if (isset($module_mark)) {$local_path='../../';}
 
-if (file_exists($local_path."cache/language.php")) {
-   include ($local_path."cache/language.php");
+if (file_exists($local_path.'cache/language.php')) {
+   include ($local_path.'cache/language.php');
 } else {
-   include ($local_path."manuels/list.php");
+   include ($local_path.'manuels/list.php');
 }
 if (isset($choice_user_language)) {
-   if ($choice_user_language!="") {
+   if ($choice_user_language!='') {
       if ($user_cook_duration<=0) {$user_cook_duration=1;}
       $timeX=time()+(3600*$user_cook_duration);
-      if ((stristr($languageslist,$choice_user_language)) and ($choice_user_language!=" ")) {
-         setcookie("user_language","$choice_user_language",$timeX);
+      if ((stristr($languageslist,$choice_user_language)) and ($choice_user_language!=' ')) {
+         setcookie('user_language',$choice_user_language,$timeX);
          $user_language=$choice_user_language;
       }
    }
 }
 if ($multi_langue) {
-   if (($user_language!="") and ($user_language!=" ")) {
+   if (($user_language!='') and ($user_language!=" ")) {
       $tmpML=stristr($languageslist,$user_language);
-      $tmpML=explode(" ",$tmpML);
+      $tmpML=explode(' ',$tmpML);
       if ($tmpML[0]) {
          $language=$tmpML[0];
       }
