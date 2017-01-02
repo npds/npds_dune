da13f87232da31510e92960e108c69cca539f8d6
jpb <jean.barbary@wanadoo.fr>
2017-01-02 10:31:19 +0100

    (c) -2017 update

--- revolution_16/sample.proxy.conf.php
+++ revolution_16/sample.proxy.conf.php
@@ -3,7 +3,7 @@
 /* DUNE by NPDS                                                         */
 /* ===========================                                          */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2010 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2017 by Philippe Brunier                     */
 /*                                                                      */
 /************************************************************************/
 /* proxy.conf.php Config for Headlines                                  */
