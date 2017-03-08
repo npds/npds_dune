efb9a442e738d578f1d194f01fa0563449f68f76
jpb <jean.barbary@wanadoo.fr>
2017-03-08 13:32:28 +0100

    v7 codeclean + mep

--- revolution_16/modules/sform/extend-user/aff_formulaire.php
+++ revolution_16/modules/sform/extend-user/aff_formulaire.php
@@ -10,7 +10,6 @@
 /************************************************************************/
 /* Dont modify this file if you dont know what you make                 */
 /************************************************************************/
-//$m->add_title(translate("User"));
 $m->add_form_field_size(50);
 
 settype($op,'string');
@@ -24,44 +23,18 @@ if (($op=="userinfo") and ($user)) {
 $m->add_field('name', translate("Identity"),$name,'text',false,60,'','');
 $m->add_field('email', translate("Real Email"),$email,'text',true,60,'','');
 settype($url,'string');
-$url="<a href=\"$url\" target=\"_blank\">$url</a>";
+$url='<a href="'.$url.'" target="_blank">'.$url.'</a>';
 $m->add_field('url',  translate("HomePage"),$url,'text',false,100,'','');
-/*
-// ---- AVATAR
-if ($smilies) {
-   $m->add_field('user_avatarX', translate("Avatar"),'','text',true,25,'','');
-   if (stristr($user_avatar,"users_private")) {
-      $direktori='';
-   } else {
-      global $theme;
-      $direktori="images/forum/avatar/";
-      if (function_exists("theme_image")) {
-         if (theme_image("forum/avatar/blank.gif"))
-            $direktori="themes/$theme/images/forum/avatar/";
-      }
-   }
-   if ($user_avatar)
-      $m->add_extender("user_avatarX", '', "<img src=\"".$direktori.$user_avatar."\" name=\"avatar\" align=\"top\" title=\"\" />");
-}
-// ---- AVATAR
-
-*/
 if($user_from!='')
-$m->add_field('user_from', translate("Location"),$user_from,'text',false,100,'','');
+   $m->add_field('user_from', translate("Location"),$user_from,'text',false,100,'','');
 if($user_occ!='')
-$m->add_field('user_occ', translate("Occupation"),$user_occ,'text',false,100,'','');
+   $m->add_field('user_occ', translate("Occupation"),$user_occ,'text',false,100,'','');
 if($user_intrest!='')
-$m->add_field('user_intrest', translate("Interest"),$user_intrest,'text',false,150,'','');
-//$m->add_field('user_sig',translate("Signature"),$user_sig,'textarea',false,255,7,'','');
-
+   $m->add_field('user_intrest', translate("Interest"),$user_intrest,'text',false,150,'','');
 if ($op=='userinfo' and $bio!='')
    $m->add_field('bio',translate("Extra Info"),$bio,'textarea',false,255,7,'','');
-
-
 if($C7!='')
    $m->add_field('C7','Latitude',$C7,'text',false,100,'','','');
 if($C8!='')
    $m->add_field('C8','Longitude',$C8,'text',false,100,'','','');
-
-
 ?>
