31515b1a9bad744c79bf8c470a2aca7c5ccd16d5
jpb <jean.barbary@wanadoo.fr>
2017-03-08 13:33:45 +0100

    v7 admin menu + mep + code minor

--- revolution_16/modules/ipban/setban.php
+++ revolution_16/modules/ipban/setban.php
@@ -20,17 +20,18 @@ $f_titre = ipban_translate("Administration de l'IpBan");
 
 function ConfigureBan($ModPath, $ModStart) {
 global $f_meta_nom, $f_titre, $adminimg,$language;
-   if (file_exists("slogs/spam.log")) {
-      $fd = fopen ("slogs/spam.log", "r");
+   if (file_exists('slogs/spam.log')) {
+      $fd = fopen ('slogs/spam.log', 'r');
       while (!feof($fd)) {
          $ip_ban.= fgets($fd, 4096);
       }
       fclose ($fd);
    }
-adminhead ($f_meta_nom, $f_titre, $adminimg);
+   GraphicAdmin($hlpfile);
+   adminhead ($f_meta_nom, $f_titre, $adminimg);
 echo '
    <hr />
-      <div class="card card-block">
+      <div class="card card-block mb-3">
          '.ipban_translate("Chaque ligne ne doit contenir qu'une adresse IP de la forme : a.b.c.d:X (ex : 168.192.1.1:5).").'<br />
          <span class="text-danger">'.ipban_translate("Si X >= 5 alors l'accès sera refusé !").'</span><br />
          '.ipban_translate("Ce fichier est mis à jour automatiquement par l'anti-spam de NPDS.").'
@@ -56,7 +57,7 @@ echo '
 }
 
 function SaveSetBan($Xip_ban) {
-   $file = fopen("slogs/spam.log", "w");
+   $file = fopen('slogs/spam.log', 'w');
       fwrite($file, $Xip_ban);
    fclose($file);
    SC_clean();
