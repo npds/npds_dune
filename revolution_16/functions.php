b11c62ce639cd75d55fe29804bf967d0048f04b3
jpb <jean.barbary@wanadoo.fr>
2017-05-10 19:39:36 +0200

    fonctions de paginations

--- revolution_16/functions.php
+++ revolution_16/functions.php
@@ -861,6 +861,100 @@ function sub_forum_folder($forum) {
    return ($ibid);
 }
 
+function paginate_single($url, $urlmore, $total, $current, $adj=3, $topics_per_page, $start) {
+   $prev = $current - 1; // page précédente
+   $next = $current + 1; // page suivante
+   $penultimate = $total - 1; //avant-dernière page
+   $pagination = '';
+   if ($total > 1) {
+      $pagination .= '
+      <nav class="my-2">
+      <ul class="pagination pagination-sm d-flex flex-wrap mt-3">';
+      if ($current == 2) {
+         $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$urlmore.'" title="'.translate("Previous Page").'" data-toggle="tooltip">◄</a></li>';
+      } elseif ($current > 2) {
+         $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$prev.$urlmore.'" title="'.translate("Previous Page").'" data-toggle="tooltip">◄</a></li>';
+      } else {
+         $pagination .= '<li class="page-item disabled"><a class="page-link" href="#">◄</a></li>';
+      }
+
+      /*
+       * Début affichage des pages, l'exemple reprend le cas de 3 numéros de pages adjacents (par défaut) de chaque côté du numéro courant
+       * - CAS 1 : il y a au plus 12 pages, insuffisant pour faire une troncature
+       * - CAS 2 : il y a au moins 13 pages, on effectue la troncature pour afficher 11 numéros de pages au total
+       */
+
+      //  CAS 1 : au plus 12 pages -> pas de troncature
+      if ($total < 7 + ($adj * 2)) {
+         $pagination .= ($current == 1) ? '<li class="page-item active"><a class="page-link" href="#">1</a></li>' : '<li class="page-item"><a class="page-link" href="'.$url.$urlmore.'">1</a></li>';
+         for ($i=2; $i<=$total; $i++) {
+            if ($i == $current) {
+               $pagination .= '<li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
+            } else {
+               $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$i.$urlmore.'">'.$i.'</a></li>';
+            }
+         }
+      }
+      //  CAS 2 : au moins 13 pages -> troncature
+      else {
+         /* Troncature 1 : 1 2 3 4 5 6 7 8 9 … 16 17 */
+         if ($current < 2 + ($adj * 2)) {
+            $pagination .= ($current == 1) ? '<li class="page-item active"><a class="page-link" href="#">1</a></li>' : '<li class="page-item"><a class="page-link" href="'.$url.'">1</a></li>';
+            for ($i = 2; $i < 4 + ($adj * 2); $i++) {
+               if ($i == $current) 
+                  $pagination .= '<li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
+               else 
+                  $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$i.$urlmore.'">'.$i.'</a></li>';
+            }
+            $pagination .= '
+            <li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.$penultimate.$urlmore.'">'.$penultimate.'</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.$total.$urlmore.'">'.$total.'</a></li>';
+         }
+         /* Troncature 2 : 1 2 … 5 6 7 8 9 10 11 … 16 17 */
+         elseif ( (($adj * 2) + 1 < $current) && ($current < $total - ($adj * 2)) ) {
+            $pagination .= 
+            '<li class="page-item"><a class="page-link" href="'.$url.'1'.$urlmore.'">1</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.'2'.$urlmore.'">2</a></li>
+            <li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
+            for ($i = ($current - $adj); $i <= $current + $adj; $i++) {
+               if ($i == $current) 
+                  $pagination .= '<li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
+               else 
+                  $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$i.$urlmore.'">'.$i.'</a></li>';
+            }
+            $pagination .= '
+            <li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.$penultimate.$urlmore.'">'.$penultimate.'</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.$total.$urlmore.'">'.$total.'</a></li>';
+         }
+         /* Troncature 3 : 1 2 … 9 10 11 12 13 14 15 16 17 */
+         else {
+            $pagination .= '
+            <li class="page-item"><a class="page-link" href="'.$url.'1'.$urlmore.'">1</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.'2'.$urlmore.'">2</a></li>
+            <li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
+            for ($i = $total - (2 + ($adj * 2)); $i <= $total; $i++) {
+               if ($i == $current) 
+                  $pagination .= '
+            <li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
+                else 
+                  $pagination .= '
+            <li class="page-item"><a class="page-link" href="'.$url.$i.$urlmore.'">'.$i.'</a></li>';
+            }
+         }
+      }
+      if ($current == $total)
+         $pagination .= '<li class="page-item disabled"><a class="page-link" href="#">►</a></li>';
+      else
+         $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$next.$urlmore.'" title="'.translate("Next Page").'" data-toggle="tooltip">►</a></li>';
+      $pagination .= '
+      </ul>
+   </nav>';
+   }
+   return ($pagination);
+}
+
 function paginate($url, $urlmore, $total, $current, $adj=3, $topics_per_page, $start) {
    $prev = $start - $topics_per_page; // page précédente
    $next = $start + $topics_per_page; // page suivante
@@ -871,7 +965,7 @@ function paginate($url, $urlmore, $total, $current, $adj=3, $topics_per_page, $s
       <nav class="my-2">
       <ul class="pagination pagination-sm d-flex flex-wrap mt-3">';
       if ($current == 1) {
-         $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$urlmore.'" title="'.translate("Previous Page").'" data-toggle="tooltip">◄</a></li>';
+         $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.'0'.$urlmore.'" title="'.translate("Previous Page").'" data-toggle="tooltip">◄</a></li>';
       } elseif ($current > 1) {
          $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$prev.$urlmore.'" title="'.translate("Previous Page").'" data-toggle="tooltip">◄</a></li>';
       } else {
@@ -886,7 +980,7 @@ function paginate($url, $urlmore, $total, $current, $adj=3, $topics_per_page, $s
 
       //  CAS 1 : au plus 12 pages -> pas de troncature
       if ($total < 7 + ($adj * 2)) {
-         $pagination .= ($current == 0) ? '<li class="page-item active"><a class="page-link" href="#">1</a></li>' : '<li class="page-item"><a class="page-link" href="'.$url.$urlmore.'">1</a></li>';
+         $pagination .= ($current == 0) ? '<li class="page-item active"><a class="page-link" href="#">1</a></li>' : '<li class="page-item"><a class="page-link" href="'.$url.'0'.$urlmore.'">1</a></li>';
          for ($i=2; $i<=$total; $i++) {
             if ($i == $current+1) {
                $pagination .= '<li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
@@ -897,31 +991,25 @@ function paginate($url, $urlmore, $total, $current, $adj=3, $topics_per_page, $s
       }
       //  CAS 2 : au moins 13 pages -> troncature
       else {
-         /**
-          * Troncature 1 : on se situe dans la partie proche des premières pages, on tronque donc la fin de la pagination.
-          * l'affichage sera de neuf numéros de pages à gauche ... deux à droite
-          * 1 2 3 4 5 6 7 8 9 … 16 17
-          */
+         /* Troncature 1 : 1 2 3 4 5 6 7 8 9 … 16 17 */
          if ($current < 2 + ($adj * 2)) {
-            $pagination .= ($current == 0) ? '<li class="page-item active"><a class="page-link" href="#">1</a></li>' : '<li class="page-item"><a class="page-link" href="'.$url.'">1</a></li>';
+            $pagination .= ($current == 0) ? '<li class="page-item active"><a class="page-link" href="#">1</a></li>' : '<li class="page-item"><a class="page-link" href="'.$url.'0'.$urlmore.'">1</a></li>';
             for ($i = 2; $i < 4 + ($adj * 2); $i++) {
                if ($i == $current+1) 
-                  $pagination .= '<li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
+                  $pagination .= '
+            <li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
                else 
-                  $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.(($i*$topics_per_page)-$topics_per_page).$urlmore.'">'.$i.'</a></li>';
+                  $pagination .= '
+            <li class="page-item"><a class="page-link" href="'.$url.(($i*$topics_per_page)-$topics_per_page).$urlmore.'">'.$i.'</a></li>';
             }
-            $pagination .= '<li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
-            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.(($penultimate*$topics_per_page)-$topics_per_page).$urlmore.'">'.$penultimate.'</a></li>';
-            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.(($total*$topics_per_page)-$topics_per_page).$urlmore.'">'.$total.'</a></li>';
+            $pagination .= '
+            <li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.(($penultimate*$topics_per_page)-$topics_per_page).$urlmore.'">'.$penultimate.'</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.(($total*$topics_per_page)-$topics_per_page).$urlmore.'">'.$total.'</a></li>';
          }
-         /**
-          * Troncature 2 : on se situe dans la partie centrale de notre pagination, on tronque donc le début et la fin de la pagination.
-          * l'affichage sera deux numéros de pages à gauche ... sept au centre ... deux à droite
-          * 1 2 … 5 6 7 8 9 10 11 … 16 17
-          */
+         /* Troncature 2 : 1 2 … 5 6 7 8 9 10 11 … 16 17 */
          elseif ( (($adj * 2) + 1 < $current) && ($current < $total - ($adj * 2)) ) {
-            // Affichage des numéros 1 et 2
-            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$urlmore.'">1</a></li>';
+            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.'0'.$urlmore.'">1</a></li>';
             $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$topics_per_page.$urlmore.'">2</a></li>';
             $pagination .= '<li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
             // les pages du milieu : les trois précédant la page courante, la page courante, puis les trois lui succédant
@@ -931,19 +1019,17 @@ function paginate($url, $urlmore, $total, $current, $adj=3, $topics_per_page, $s
                else 
                   $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.(($i*$topics_per_page)-$topics_per_page).$urlmore.'">'.$i.'</a></li>';
             }
-            $pagination .= '<li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
-            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.(($penultimate*$topics_per_page)-$topics_per_page).$urlmore.'">'.$penultimate.'</a></li>';
-            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.(($total*$topics_per_page)-$topics_per_page).$urlmore.'">'.$total.'</a></li>';
+            $pagination .= '
+            <li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.(($penultimate*$topics_per_page)-$topics_per_page).$urlmore.'">'.$penultimate.'</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.(($total*$topics_per_page)-$topics_per_page).$urlmore.'">'.$total.'</a></li>';
          }
-         /**
-          * Troncature 3 : on se situe dans la partie de droite, on tronque donc le début de la pagination.
-          * l'affichage sera deux numéros de pages à gauche ... neuf à droite
-          * 1 2 … 9 10 11 12 13 14 15 16 17
-          */
+         /* Troncature 3 : 1 2 … 9 10 11 12 13 14 15 16 17 */
          else {
-            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$urlmore.'">1</a></li>';
-            $pagination .= '<li class="page-item"><a class="page-link" href="'.$url.$topics_per_page.$urlmore.'">2</a></li>';
-            $pagination .= '<li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
+            $pagination .= '
+            <li class="page-item"><a class="page-link" href="'.$url.'0'.$urlmore.'">1</a></li>
+            <li class="page-item"><a class="page-link" href="'.$url.$topics_per_page.$urlmore.'">2</a></li>
+            <li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
             for ($i = $total - (2 + ($adj * 2)); $i <= $total; $i++) {
                if ($i == $current+1) 
                   $pagination .= '<li class="page-item active"><a class="page-link" href="#">'.$i.'</a></li>';
