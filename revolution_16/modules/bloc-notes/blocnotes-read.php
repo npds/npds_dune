dcd62b3c092ffd29a85a22802d614a12d122a097
jeanpierre <jean.barbary@wanadoo.fr>
2017-06-04 11:41:05 +0200

    bloc note clean + code

--- revolution_16/modules/bloc-notes/blocnotes-read.php
+++ revolution_16/modules/bloc-notes/blocnotes-read.php
@@ -5,7 +5,7 @@
 /*                                                                      */
 /* BLOC-NOTES engine for NPDS - Philippe Brunier & Arnaud Latourrette   */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2009 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2017 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -15,10 +15,12 @@
 if (strstr($bnid,"..") || strstr($bnid,"./") || stristr($bnid, "script") || stristr($bnid, "cookie") || stristr($bnid, "iframe") || stristr($bnid, "applet") || stristr($bnid, "object") || stristr($bnid, "meta") ) {
    die();
 }
-$result = sql_query("select texte from ".$NPDS_Prefix."blocnotes where bnid='$bnid'");
+$result = sql_query("SELECT texte FROM ".$NPDS_Prefix."blocnotes WHERE bnid='$bnid'");
 if (sql_num_rows($result) > 0) {
    list($texte)=sql_fetch_row($result);
 }
-$texte=StripSlashes($texte);
-echo "document.write('<script>document.A$bnid.texteBlocNote.value=unescape(\'".str_replace(chr(13).chr(10),"\\\\n",str_replace("'","\\\\\'",$texte))."\');</script>')";
+$texte=stripslashes($texte);
+$texte = str_replace(chr(13).chr(10),"\\n",str_replace("'","\'",$texte));
+//echo "document.write('<script type=\"text/javascript\">document.A$bnid.texteBlocNote.value=unescape(\'".str_replace(chr(13).chr(10),"\\\\n",str_replace("'","\\\\\'",$texte))."\');</script>')";
+echo '$(function(){ $("#texteBlocNote_'.$bnid.'").val(unescape("'.$texte.'")) })';
 ?>
