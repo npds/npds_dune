8a5099c60f450806a41644204684fada54eb4bea
jeanpierre <jean.barbary@wanadoo.fr>
2017-07-06 09:37:21 +0200

    #183
    les notices généré perturbe l’affichage… éradications des notices quand
    $user n’est pas défini et c ok ….

--- revolution_16/grab_globals.php
+++ revolution_16/grab_globals.php
@@ -20,11 +20,10 @@
 if (!defined('NPDS_GRAB_GLOBALS_INCLUDED')) {
     define('NPDS_GRAB_GLOBALS_INCLUDED', 1);
 
-   // Modify the report level off PHP
+   // Modify the report level of PHP
    // error_reporting(0);// report NO ERROR
-   //error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); // Devel report
-   error_reporting(E_ERROR | E_WARNING | E_PARSE); // standard ERROR report
-    //error_reporting(E_ERROR | E_WARNING | E_PARSE);
+   error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); // Devel report
+//   error_reporting(E_ERROR | E_WARNING | E_PARSE); // standard ERROR report
 
     function getip() {
        if (isset($_SERVER)) {
