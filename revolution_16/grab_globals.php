3571663cac662bfae598f42356a84acfd3e6e188
jeanpierre <jean.barbary@wanadoo.fr>
2017-07-07 09:53:54 +0200

    prod mod

--- revolution_16/grab_globals.php
+++ revolution_16/grab_globals.php
@@ -22,8 +22,8 @@ if (!defined('NPDS_GRAB_GLOBALS_INCLUDED')) {
 
    // Modify the report level of PHP
    // error_reporting(0);// report NO ERROR
-   error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); // Devel report
-//   error_reporting(E_ERROR | E_WARNING | E_PARSE); // standard ERROR report
+   //error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); // Devel report
+   error_reporting(E_ERROR | E_WARNING | E_PARSE); // standard ERROR report
 
     function getip() {
        if (isset($_SERVER)) {
