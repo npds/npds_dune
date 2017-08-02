7fd7d7ff3ec890436bee8809a0cd63b3e2b89928
jeanpierre <jean.barbary@wanadoo.fr>
2017-08-02 23:54:10 +0200

    change class and lib

--- revolution_16/lib/js/npds_adapt.js
+++ revolution_16/lib/js/npds_adapt.js
@@ -14,35 +14,34 @@
          };
       // <== choix icon boostrap table //
 
-      $(document).ready(function(){
-         // responsive old data
-         $(".article_texte img,.ibid_descr img").addClass("img-fluid");
-         $("#ban img,#art_preview img,#online_user_journal img,#art_sect img").addClass("img-fluid");
-         $("iframe").addClass("embed-responsive-item");
+         $(document).ready(function(){
+            // responsive old data
+            $('.article_texte img,.ibid_descr img').addClass('img-fluid');
+            $('#ban img,#art_preview img,#online_user_journal img,#art_sect img').addClass('img-fluid');
+            $('iframe').addClass("embed-responsive-item");
          
-         $(".fo-post-mes img").addClass("img-fluid");//not sure if usefull to late ...
-         // icon toggle 
-         $('a[data-toggle="collapse"]').click(function () {
-            $(this).find('i.toggle-icon').toggleClass('fa-caret-up fa-caret-down',6000);
-         })
-         $('a[data-toggle="collapse"]').click(function () {
-            $(this).find('i.togglearbr-icon').toggleClass('fa-level-up fa-level-down',6000);
-         })
-         // initialisation tooltip et popover (qui ferme au prochain click)
-         $('[data-toggle="tooltip"]').tooltip({container:'body'});
-         $('[data-toggle="popover"]').popover();
-         $('.popover-dismiss').popover({ trigger: 'focus'})
-
-         // fix bug tooltip in table
-         $('table').on('all.bs.table', function (e, name, args) {
-            $('[data-toggle="tooltip"]').tooltip();
+            $('.fo-post-mes img').addClass('img-fluid');//not sure if usefull to late ...
+            // icon toggle 
+            $('a[data-toggle="collapse"]').click(function () {
+               $(this).find('i.toggle-icon').toggleClass('fa-caret-up fa-caret-down',6000);
+            })
+            $('a[data-toggle="collapse"]').click(function () {
+               $(this).find('i.togglearbr-icon').toggleClass('fa-level-up fa-level-down',6000);
+            })
+            // initialisation tooltip et popover (qui ferme au prochain click)
+            $('[data-toggle="tooltip"]').tooltip({container:'body'});
             $('[data-toggle="popover"]').popover();
+            $('.popover-dismiss').popover({ trigger: 'focus'})
+
+            // fix bug tooltip in table
+            $('table').on('all.bs.table', function (e, name, args) {
+               $('[data-toggle="tooltip"]').tooltip();
+               $('[data-toggle="popover"]').popover();
+            });
+            // ==> colorisation syntaxique du code //
+            $('pre code').each(function(i, block) {
+               hljs.highlightBlock(block);
+               hljs.configure({useBR: true});
+            });
          });
-      });
-      
-      // ==> colorisation syntaxique du code //
-      $('pre code').each(function(i, block) {
-        hljs.highlightBlock(block);
-        hljs.configure({useBR: true});
-      });
       //]]>
