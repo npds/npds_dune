bce7aa1026f5c0faa6a9c90926af7f17cef32467
jeanpierre <jean.barbary@wanadoo.fr>
2017-09-01 10:51:58 +0200

    is back

---
+++ revolution_16/sections.config.php
@@ -0,0 +1,15 @@
+<?php
+
+# # DUNE by NPDS : Net Portal Dynamic System
+# ===================================================
+#
+# This version name NPDS Copyright (c) 2001-2017 by Philippe Brunier
+#
+# This module is to configure the Sections 
+#
+# This program is free software. You can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License.
+
+$sections_chemin=1;
+?>
