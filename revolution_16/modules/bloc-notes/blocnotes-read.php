a4a439f707f80f5a62bedb5c86a98a61389d716f
jeanpierre <jean.barbary@wanadoo.fr>
2017-09-12 20:22:32 +0200

    notice
    plus de notice mais à revoir car le unescape en js ??… deprecié et
    compacter le traitement de $texte

--- revolution_16/modules/bloc-notes/blocnotes-read.php
+++ revolution_16/modules/bloc-notes/blocnotes-read.php
@@ -12,15 +12,13 @@
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 
-if (strstr($bnid,"..") || strstr($bnid,"./") || stristr($bnid, "script") || stristr($bnid, "cookie") || stristr($bnid, "iframe") || stristr($bnid, "applet") || stristr($bnid, "object") || stristr($bnid, "meta") ) {
+if (strstr($bnid,'..') || strstr($bnid,'./') || stristr($bnid, 'script') || stristr($bnid, 'cookie') || stristr($bnid, 'iframe') || stristr($bnid, 'applet') || stristr($bnid, 'object') || stristr($bnid, 'meta') )
    die();
-}
 $result = sql_query("SELECT texte FROM ".$NPDS_Prefix."blocnotes WHERE bnid='$bnid'");
 if (sql_num_rows($result) > 0) {
    list($texte)=sql_fetch_row($result);
+   $texte=stripslashes($texte);
+   $texte = str_replace(chr(13).chr(10),"\\n",str_replace("'","\'",$texte));
+   echo '$(function(){ $("#texteBlocNote_'.$bnid.'").val(unescape("'.str_replace('"','\\"',$texte).'")); })';
 }
-$texte=stripslashes($texte);
-$texte = str_replace(chr(13).chr(10),"\\n",str_replace("'","\'",$texte));
-//echo "document.write('<script type=\"text/javascript\">document.A$bnid.texteBlocNote.value=unescape(\'".str_replace(chr(13).chr(10),"\\\\n",str_replace("'","\\\\\'",$texte))."\');</script>')";
-echo '$(function(){ $("#texteBlocNote_'.$bnid.'").val(unescape("'.$texte.'")) })';
 ?>
