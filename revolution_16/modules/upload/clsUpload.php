0f266b3b6e1083b4c69cc373e3185342628b2d50
jeanpierre <jean.barbary@wanadoo.fr>
2018-09-17 18:16:42 +0200

    for php7++ notice class with same name of fonction

--- revolution_16/modules/upload/clsUpload.php
+++ revolution_16/modules/upload/clsUpload.php
@@ -1,7 +1,7 @@
 <?php
 /*
  ************************************************************************
- * © Sloppycode.net All rights reserved.
+ * Â© Sloppycode.net All rights reserved.
  *
  * This is a standard copyright header for all source code appearing
  * at sloppycode.net. This application/class/script may be redistributed,
@@ -29,65 +29,68 @@
  */
 
 class Upload {
-        var $maxupload_size;
-        var $errors;
-        var $isPosted;
-        var $HTTP_POST_FILES;
+   var $maxupload_size;
+   var $errors;
+   var $isPosted;
+   var $HTTP_POST_FILES;
 
-        function Upload() {
-           global $HTTP_POST_FILES, $_FILES;
+   public function __construct() {
+      global $HTTP_POST_FILES, $_FILES;
 
-           if (!empty($HTTP_POST_FILES))
-              $fic=$HTTP_POST_FILES;
-           else
-              $fic=$_FILES;
+      if (!empty($HTTP_POST_FILES))
+         $fic=$HTTP_POST_FILES;
+      else
+         $fic=$_FILES;
 
-           $this->HTTP_POST_FILES = $fic;
+      $this->HTTP_POST_FILES = $fic;
 
-           if (empty($fic)) {
-              $this->isPosted = false;
-           } else {
-              $this->isPosted = true;
-           }
-        }
+      if (empty($fic))
+         $this->isPosted = false;
+      else
+         $this->isPosted = true;
+   }
 
-        function saveAs($filename, $directory, $field, $overwrite,$mode=0766) {
-           if ($this->isPosted) {
-              if ($this->HTTP_POST_FILES[$field]['size'] < $this->maxupload_size && $this->HTTP_POST_FILES[$field]['size'] >0) {
-                 $noerrors = true;
-                 $tempName = $this->HTTP_POST_FILES[$field]['tmp_name'];
-                 $all      = $directory.$filename;
-                 if (file_exists($all)) {
-                    if ($overwrite) {
-                       @unlink($all) || $noerrors=false; $this->errors  = upload_translate("Erreur de téléchargement du fichier - fichier non sauvegardé.");
-                       @move_uploaded_file($tempName,$all) || $noerrors=false; $this->errors .= upload_translate("Erreur de téléchargement du fichier - fichier non sauvegardé.");
-                       @chmod($all,$mode);
-                    }
-                 } else {
-                    @move_uploaded_file($tempName,$all) || $noerrors=false; $this->errors  = upload_translate("Erreur de téléchargement du fichier - fichier non sauvegardé.");
-                    @chmod($all,$mode);
-                 }
-                 return $noerrors;
-              } elseif ($this->HTTP_POST_FILES[$field]['size'] > $this->maxupload_size) {
-                 $this->errors = upload_translate("La taille de ce fichier excède la taille maximum autorisée")." => ".number_format(($this->maxupload_size/1024),2)." Kbs";
-                 return false;
-              } elseif ($this->HTTP_POST_FILES[$field]['size'] == 0) {
-                 $this->errors = upload_translate("Erreur de téléchargement du fichier - fichier non sauvegardé.");
-                 return false;
-              }
-           }
-        }
+   public function Upload() {
+      self::__construct();
+   }
 
-        function getFilename($field) {
-           return $this->HTTP_POST_FILES[$field]['name'];
-        }
+   function saveAs($filename, $directory, $field, $overwrite,$mode=0766) {
+      if ($this->isPosted) {
+         if ($this->HTTP_POST_FILES[$field]['size'] < $this->maxupload_size && $this->HTTP_POST_FILES[$field]['size'] >0) {
+            $noerrors = true;
+            $tempName = $this->HTTP_POST_FILES[$field]['tmp_name'];
+            $all      = $directory.$filename;
+            if (file_exists($all)) {
+               if ($overwrite) {
+                  @unlink($all) || $noerrors=false; $this->errors  = upload_translate("Erreur de tÃ©lÃ©chargement du fichier - fichier non sauvegardÃ©.");
+                  @move_uploaded_file($tempName,$all) || $noerrors=false; $this->errors .= upload_translate("Erreur de tÃ©lÃ©chargement du fichier - fichier non sauvegardÃ©.");
+                  @chmod($all,$mode);
+               }
+            } else {
+               @move_uploaded_file($tempName,$all) || $noerrors=false; $this->errors  = upload_translate("Erreur de tÃ©lÃ©chargement du fichier - fichier non sauvegardÃ©.");
+               @chmod($all,$mode);
+            }
+            return $noerrors;
+         } elseif ($this->HTTP_POST_FILES[$field]['size'] > $this->maxupload_size) {
+            $this->errors = upload_translate("La taille de ce fichier excÃ¨de la taille maximum autorisÃ©e")." => ".number_format(($this->maxupload_size/1024),2)." Kbs";
+            return false;
+         } elseif ($this->HTTP_POST_FILES[$field]['size'] == 0) {
+            $this->errors = upload_translate("Erreur de tÃ©lÃ©chargement du fichier - fichier non sauvegardÃ©.");
+            return false;
+         }
+      }
+   }
 
-        function getFileMimeType($field) {
-           return $this->HTTP_POST_FILES[$field]['type'];
-        }
+   function getFilename($field) {
+      return $this->HTTP_POST_FILES[$field]['name'];
+   }
 
-        function getFileSize($field) {
-           return $this->HTTP_POST_FILES[$field]['size'];
-        }
+   function getFileMimeType($field) {
+      return $this->HTTP_POST_FILES[$field]['type'];
+   }
+
+   function getFileSize($field) {
+      return $this->HTTP_POST_FILES[$field]['size'];
+   }
 }
 ?>
