074671cacacf8d2d9f5d364f46a7cd40c62339a6
jeanpierre <jean.barbary@wanadoo.fr>
2018-10-20 22:00:06 +0200

    current dev version

--- revolution_16/admin/settings_save.php
+++ revolution_16/admin/settings_save.php
@@ -562,7 +562,7 @@ function ConfigSave($xparse,$xsitename,$xnuke_url,$xsite_logo,$xslogan,$xstartda
     $content .= "\$NPDS_Prefix = \"$NPDS_Prefix\";\n";
     if ($NPDS_Key=="") {$NPDS_Key=uniqid("");}
     $content .= "\$NPDS_Key = \"$NPDS_Key\";\n";
-    $content .= "\$Version_Num = \"v.16.0.2-beta\";\n";
+    $content .= "\$Version_Num = \"v.16.0.3-beta\";\n";
     $content .= "\$Version_Id = \"NPDS\";\n";
     $content .= "\$Version_Sub = \"REvolution\";\n";
     $content .= "\n";
