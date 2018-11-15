c8e86250b68b44aafdcc0cba243197fedfb475f4
jeanpierre <jean.barbary@wanadoo.fr>
2018-11-15 20:56:16 +0100

    maj tac ...

--- revolution_16/modules/upload/upload_forum.php
+++ revolution_16/modules/upload/upload_forum.php
@@ -17,7 +17,36 @@ function win_upload($apli,$IdPost,$IdForum,$IdTopic,$typeL) {
       //<![CDATA[
       window.open('modules.php?ModPath=upload&ModStart=include_forum/upload_forum2&apli=$apli&IdPost=$IdPost&IdForum=$IdForum&IdTopic=$IdTopic','wtmpForum', 'menubar=no,location=no,directories=no,status=no,copyhistory=no,toolbar=no,scrollbars=yes,resizable=yes, width=640, height=480');
       //]]>
-      </script>\n";
+      </script>";
+   else if($typeL=='themodal')
+      echo '
+      <div class="modal fade" id="'.$typeL.'" tabindex="-1" role="dialog">
+         <div class="modal-dialog" role="document">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">'.translate("Files").'</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+               </div>
+               <div class="modal-body"></div>
+               <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+      <a class="mr-3" href="#themodal" data-remote="modules.php?ModPath=upload&ModStart=include_forum/upload_forum2&apli='.$apli.'&IdPost='.$IdPost.'&IdForum='.$IdForum.'&IdTopic='.$IdTopic.'" data-toggle="modal" data-target="#themodal" title="'.translate("Files").'" data-toggle="tooltip"><i class="fa fa-download fa-lg"></i></a>
+
+      
+      <script type="text/javascript">
+      //<![CDATA[
+         $("#'.$typeL.'").on("show.bs.modal", function (e) {
+             var button = $(e.relatedTarget);
+             var modal = $(this);
+             modal.find(".modal-body").load(button.data("remote"));
+         });
+      //]]>
+      </script>';
    else
       return ("'modules.php?ModPath=upload&ModStart=include_forum/upload_forum2&apli=$apli&IdPost=$IdPost&IdForum=$IdForum&IdTopic=$IdTopic','wtmpForum', 'menubar=no,location=no,directories=no,status=no,copyhistory=no,toolbar=no,scrollbars=yes,resizable=yes, width=640, height=480'");
 }
