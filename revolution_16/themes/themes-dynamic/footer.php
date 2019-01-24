fc25e1dbeff872abb27352baa1669cc9e4c641ed
jeanpierre <jean.barbary@wanadoo.fr>
2019-01-24 15:40:34 +0100

    year + code typo

--- revolution_16/themes/themes-dynamic/footer.php
+++ revolution_16/themes/themes-dynamic/footer.php
@@ -4,7 +4,7 @@
 /* ===========================                                          */
 /*                                                                      */
 /* DYNAMIC THEME engine for NPDS                                        */
-/* NPDS Copyright (c) 2002-2018 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2019 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -12,20 +12,22 @@
 /************************************************************************/
 global $theme; $rep=false;
 settype($ContainerGlobal,'string');
-if (file_exists("themes/".$theme."/html/footer.html")) $rep=$theme;
-elseif (file_exists("themes/default/html/footer.html")) $rep="default";
+if (file_exists("themes/".$theme."/html/footer.html"))
+   $rep=$theme;
+elseif (file_exists("themes/default/html/footer.html"))
+   $rep="default";
 else {
-    echo "footer.html manquant / not find !<br />";
-    die();
+   echo "footer.html manquant / not find !<br />";
+   die();
 }
 
 if ($rep) {
-    ob_start();
-    include("themes/".$rep."/html/footer.html");
-    $Xcontent=ob_get_contents();
-    ob_end_clean();
-    if ($ContainerGlobal)
-       $Xcontent.=$ContainerGlobal;
-    echo meta_lang(aff_langue($Xcontent));
+   ob_start();
+   include("themes/".$rep."/html/footer.html");
+   $Xcontent=ob_get_contents();
+   ob_end_clean();
+   if ($ContainerGlobal)
+    $Xcontent.=$ContainerGlobal;
+   echo meta_lang(aff_langue($Xcontent));
 }
 ?>
