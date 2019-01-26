41a585da1f81c8a479f1cc51a39b192c74884e3c
jeanpierre <jean.barbary@wanadoo.fr>
2019-01-26 21:54:31 +0100

    year

--- revolution_16/modules/wspad/ws_verrou.php
+++ revolution_16/modules/wspad/ws_verrou.php
@@ -5,7 +5,7 @@
 /*                                                                      */
 /* Collab WS-Pad 1.44 by Developpeur and Jpb                            */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2018 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2019 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
