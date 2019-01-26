ef3c3dbc64969cd8bb2163620c6f4de817e4e63c
jeanpierre <jean.barbary@wanadoo.fr>
2019-01-26 23:20:22 +0100

    year +code

--- revolution_16/modules/meta-lang/adv-meta_lang-doc.php
+++ revolution_16/modules/meta-lang/adv-meta_lang-doc.php
@@ -3,26 +3,24 @@
 /* DUNE by NPDS                                                         */
 /* ===========================                                          */
 /*                                                                      */
-/* This version name NPDS Copyright (c) 2001-2017 by Philippe Brunier   */
+/* This version name NPDS Copyright (c) 2001-2019 by Philippe Brunier   */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
-if (!stristr($_SERVER['PHP_SELF'],"modules.php")) { die(); }
+if (!stristr($_SERVER['PHP_SELF'],"modules.php")) die();
 
    global $user, $Default_Theme, $language, $site_font;
    $userX = base64_decode($user);
    $userdata = explode(':', $userX);
    if (isset($userdata[9])) {
-      if (!$file=@opendir("themes/$userdata[9]")) {
+      if (!$file=@opendir("themes/$userdata[9]"))
          $tmp_theme=$Default_Theme;
-      } else {
+      else
          $tmp_theme=$userdata[9];
-      }
-   } else {
+   } else
       $tmp_theme=$Default_Theme;
-   }
    include("themes/$tmp_theme/theme.php");
    $Titlesitename="META-LANG";
    include("meta/meta.php");
