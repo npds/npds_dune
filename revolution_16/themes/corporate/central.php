9163a60fb8155588a07ff3ae08509286f8cb5cc5
jeanpierre <jean.barbary@wanadoo.fr>
2019-01-27 17:54:44 +0100

    code typo

--- revolution_16/themes/corporate/central.php
+++ revolution_16/themes/corporate/central.php
@@ -1,13 +1,15 @@
 <?php
-/************************************************************** **********/
-/* Modification par Jireck      Rocssti2015                                          */
+/************************************************************************/
+/* Modification par Jireck                                              */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 global $theme; $rep=false;
-if (file_exists("themes/".$theme."/html/central.html")) {$rep=$theme;}
-elseif (file_exists("themes/default/html/central.html")) {$rep="default";}
+if (file_exists("themes/".$theme."/html/central.html"))
+   $rep=$theme;
+elseif (file_exists("themes/default/html/central.html"))
+   $rep="default";
 else {
    echo "central.html manquant / not find !< br />";
    die();
