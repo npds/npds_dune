d11f44a59a5fd55a55a5af6c57ba4c43db77e99e
jeanpierre <jean.barbary@wanadoo.fr>
2019-01-31 20:34:01 +0100

    html  + icon

--- revolution_16/admin/modules.php
+++ revolution_16/admin/modules.php
@@ -60,19 +60,19 @@ global $language,$adminimg, $admf_ext;
    echo '
    <hr />
    <h3>'.adm_translate("Les modules").'</h3>
-   <table id="tad_modu" data-toggle="table" data-striped="true" data-show-toggle="true" data-mobile-responsive="true" data-buttons-class="outline-secondary" data-icons="icons" data-icons-prefix="fa">
+   <table id="tad_modu" data-toggle="table" data-striped="false" data-show-toggle="true" data-mobile-responsive="true" data-buttons-class="outline-secondary" data-icons="icons" data-icons-prefix="fa">
       <thead>
          <tr>
-            <th class="n-t-col-xs-1"><img class="adm_img" src="images/admin/module.png" alt="icon_module" /></th>
+            <th data-align="center" class="n-t-col-xs-1"><img class="adm_img" src="images/admin/module.png" alt="icon_module" /></th>
             <th data-sortable="true">'.adm_translate('Nom').'</th>
-            <th class="n-t-col-xs-2" data-align="center">'.adm_translate('Fonctions').'</th>
+            <th data-align="center" class="n-t-col-xs-2" >'.adm_translate('Fonctions').'</th>
          </tr>
       </thead>
       <tbody>';
 
    $result = sql_query("SELECT * FROM ".$NPDS_Prefix."modules ORDER BY mid");
    while ($row = sql_fetch_assoc($result)) {
-      $icomod='';
+      $icomod=''; $clatd='';
       if (file_exists("modules/".$row["mnom"]."/".$row["mnom"].".png"))
          $icomod='<img class="adm_img" src="modules/'.$row["mnom"].'/'.$row["mnom"].'.png" alt="icon_'.$row["mnom"].'" title="" />';
       else
@@ -80,17 +80,20 @@ global $language,$adminimg, $admf_ext;
 
       if ($row["minstall"] == 0) {
          if (file_exists("modules/".$row["mnom"]."/install.conf.php"))
-            $status_chngac = '<a href="admin.php?op=Module-Install&amp;ModInstall='.$row["mnom"].'" ><i class="fa fa-spinner fa-2x text-success" title="'.adm_translate("Installer le module").'" data-toggle="tooltip"></i></a>';
+            $status_chngac = '<a href="admin.php?op=Module-Install&amp;ModInstall='.$row["mnom"].'" ><i class="fa fa-compress fa-lg text-success"></i><i class="fa fa-puzzle-piece fa-2x fa-rotate-90 text-success" title="'.adm_translate("Installer le module").'" data-toggle="tooltip"></i></a>';
          else
             $status_chngac = '<a href="admin.php?op=Module-Install&amp;ModInstall='.$row["mnom"].'&amp;subop=install"><i class="fa fa-check-square-o fa-2x" title="'.adm_translate("Pas d'installeur disponible").' '.adm_translate("Marquer le module comme installé").'" data-toggle="tooltip"></i></a>';
+         $clatd='table-danger';
       }
-      else
-         $status_chngac = '<a href="admin.php?op=Module-Install&amp;ModDesinstall='.$row["mnom"].'" ><i class="fa fa-ban fa-2x text-danger" title="'.adm_translate("Désinstaller le module").'" data-toggle="tooltip"></i></a>';
+      else {
+         $status_chngac = '<a href="admin.php?op=Module-Install&amp;ModDesinstall='.$row["mnom"].'" ><i class="fa fa-expand fa-lg text-danger"></i><i class="fa fa fa-puzzle-piece fa-2x fa-rotate-90 text-danger" title="'.adm_translate("Désinstaller le module").'" data-toggle="tooltip"></i></a>';
+         $clatd='table-success';
+      }
       echo '
           <tr>
-             <td align="center">'.$icomod.'</td>
-             <td align="left">'.$row["mnom"].'</td>
-             <td align="left">'.$status_chngac.'</td>
+             <td class="'.$clatd.'">'.$icomod.'</td>
+             <td class="'.$clatd.'">'.$row["mnom"].'</td>
+             <td class="'.$clatd.'">'.$status_chngac.'</td>
           </tr>';
    }
    echo '
