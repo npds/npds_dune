437991fa00083051e5c183fcfb42db40ce1f7186
jeanpierre <jean.barbary@wanadoo.fr>
2019-09-20 09:01:19 +0200

    remove google key add ipdata key & minor change

--- revolution_16/modules/geoloc/geoloc_conf.php
+++ revolution_16/modules/geoloc/geoloc_conf.php
@@ -15,8 +15,8 @@
 /* geoloc_conf.php file 2008-2019 by Jean Pierre Barbary (jpb)          */
 /* dev team : Philippe Revilliod (Phr)                                  */
 /************************************************************************/
-$api_key = ""; // clef api google 
 $api_key_bing = ""; // clef api bing maps 
+$api_key_mapbox = ""; // clef api mapbox 
 $ch_lat = "C7"; // Champ lat dans sql 
 $ch_lon = "C8"; // Champ long dans sql 
 // interface carte 
@@ -24,14 +24,15 @@ $cartyp = "OSM"; // Type de carte
 $co_unit = "dms"; // Coordinates Units
 $ch_img = "modules/geoloc/images/"; // Chemin des images 
 $geo_ip = 0; // Autorisation de géolocalisation des IP 
+$api_key_ipdata = ""; // Clef API pour provider IP 
 $nm_img_acg = "acg.png"; // Nom fichier image anonyme géoréférencé en ligne 
 $nm_img_mbcg = "mbcg.png"; // Nom fichier image membre géoréférencé en ligne 
 $nm_img_mbg = "mbg.png"; // Nom fichier image membre géoréférencé 
 $mark_typ = 0; // Type de marker 
 $w_ico = "28"; // Largeur icone des markers 
 $h_ico = "28"; // Hauteur icone des markers
-$f_mbg = "uf007"; // Font SVG 
-$mbg_sc = "0.5"; // Echelle du Font SVG du membre 
+$f_mbg = "user"; // Font SVG 
+$mbg_sc = "24"; // Echelle du Font SVG du membre 
 $mbg_t_ep = "1"; // Epaisseur trait Font SVG du membre 
 $mbg_t_co = "rgba(10, 10, 10, 0.75)"; // Couleur trait SVG du membre 
 $mbg_t_op = "1"; // Opacité trait SVG du membre 
@@ -43,7 +44,7 @@ $mbgc_t_co = "rgb(101, 55, 55)"; // Couleur trait SVG du membre géoréférencé
 $mbgc_t_op = "1"; // Opacité trait SVG du membre géoréférencé 
 $mbgc_f_co = "rgba(233, 22, 62, 0.87)"; // Couleur fond SVG du membre géoréférencé 
 $mbgc_f_op = "1"; // Opacité fond SVG du membre géoréférencé 
-$acg_sc = "0.5"; // Echelle du Font SVG pour anonyme en ligne 
+$acg_sc = "24"; // Echelle du Font SVG pour anonyme en ligne 
 $acg_t_ep = "0"; // Epaisseur trait Font SVG pour anonyme en ligne 
 $acg_t_co = "rgb(153, 146, 142)"; // Couleur trait SVG pour anonyme en ligne 
 $acg_t_op = "1"; // Opacité trait SVG pour anonyme en ligne 
