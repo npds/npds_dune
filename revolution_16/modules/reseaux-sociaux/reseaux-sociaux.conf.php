2a2567546cdc2c8923181ffdc92ae3f294ff837a
jeanpierre <jean.barbary@wanadoo.fr>
2020-01-04 11:28:06 +0100

    fontawesome5 update

--- revolution_16/modules/reseaux-sociaux/reseaux-sociaux.conf.php
+++ revolution_16/modules/reseaux-sociaux/reseaux-sociaux.conf.php
@@ -23,7 +23,7 @@ $rs = [
 ['facebook','https://facebook.com/','facebook'],
 ['twitter','https://twitter.com/','twitter'],
 ['snapchat','https://www.snapchat.com/','snapchat'],
-['linkedin','https://www.linkedin.com/in/','linkedin-square'],
+['linkedin','https://www.linkedin.com/in/','linkedin'],
 ['github','https://github.com/','github']
 ];
 ?>
