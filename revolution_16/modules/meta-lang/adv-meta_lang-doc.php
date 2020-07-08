0eca3b12025d7f95fb771e6d3f44c6d1f75d937e
jeanpierre <jean.barbary@wanadoo.fr>
2020-07-08 18:26:11 +0200

    skinable ....+ mep

--- revolution_16/modules/meta-lang/adv-meta_lang-doc.php
+++ revolution_16/modules/meta-lang/adv-meta_lang-doc.php
@@ -3,7 +3,7 @@
 /* DUNE by NPDS                                                         */
 /* ===========================                                          */
 /*                                                                      */
-/* This version name NPDS Copyright (c) 2001-2019 by Philippe Brunier   */
+/* This version name NPDS Copyright (c) 2001-2020 by Philippe Brunier   */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -11,25 +11,31 @@
 /************************************************************************/
 if (!stristr($_SERVER['PHP_SELF'],"modules.php")) die();
 
-   global $user, $Default_Theme, $language, $site_font;
-   $userX = base64_decode($user);
-   $userdata = explode(':', $userX);
-   if (isset($userdata[9])) {
-      if (!$file=@opendir("themes/$userdata[9]"))
+   global $Default_Theme, $Default_Skin, $user, $language;
+   if (isset($user) and $user!='') {
+      global $cookie;
+      if($cookie[9] !='') {
+         $ibix=explode('+', urldecode($cookie[9]));
+         if (array_key_exists(0, $ibix)) $theme=$ibix[0]; else $theme=$Default_Theme;
+         if (array_key_exists(1, $ibix)) $skin=$ibix[1]; else $skin=$Default_skin; //$skin=''; 
+         $tmp_theme=$theme;
+         if (!$file=@opendir("themes/$theme")) $tmp_theme=$Default_Theme;
+      } else 
          $tmp_theme=$Default_Theme;
-      else
-         $tmp_theme=$userdata[9];
-   } else
-      $tmp_theme=$Default_Theme;
+   } else {
+      $theme=$Default_Theme;
+      $skin=$Default_Skin;
+      $tmp_theme=$theme;
+   }
    include("themes/$tmp_theme/theme.php");
    $Titlesitename="META-LANG";
    include("meta/meta.php");
-//   echo import_css($tmp_theme, $language, $site_font, "","");
-echo '<link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css">';// en dur lol en attendant
+   echo import_css($tmp_theme, $language, $skin, '','');
    global $NPDS_Prefix;
    $Q = sql_query("SELECT def, content, type_meta, type_uri, uri, description FROM ".$NPDS_Prefix."metalang ORDER BY 'type_meta','def' ASC");
    echo '
-   <table class="table table-striped table-responsive table-hover table-sm" border="0" >
+   <div class="p-2">
+   <table class="table table-striped table-responsive table-hover table-sm table-bordered" >
       <thead class="thead-default">
          <tr>
             <th>META</th>
@@ -39,26 +45,26 @@ echo '<link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css">';/
       </thead>';
    $cur_type=''; $ibid=0;
    while (list($def, $content, $type_meta, $type_uri, $uri, $description)= sql_fetch_row($Q)) {
-      if ($cur_type=="")
+      if ($cur_type=='')
          $cur_type=$type_meta;
       if ($type_meta!=$cur_type) {
          echo '
          </tr>
          <tr>
-            <td class="table-info" colspan="3"></td>
+            <td class="lead" colspan="3">'.$type_meta.'</td>
          </tr>
       <tbody>';
          $cur_type=$type_meta;
       }
-         if ($tiny_mce)
-            $def_modifier="<a href=\"#\" onclick=\"javascript:parent.tinymce.activeEditor.selection.setContent(' ".$def." ');top.tinymce.activeEditor.windowManager.close();
-\" >$def</a>";
+      if(isset($_SERVER['HTTP_REFERER']) and strstr($_SERVER['HTTP_REFERER'],'submit.php'))
+            $def_modifier="<a class=\"tooltipbyclass\" href=\"#\" onclick=\"javascript:parent.tinymce.activeEditor.selection.setContent(' ".$def." ');top.tinymce.activeEditor.windowManager.close();
+\" title=\"Cliquer pour utiliser ce méta-mot dans votre texte.\">$def</a>";
          else 
             $def_modifier=$def;
       echo '
          <tr>
             <td valign="top" align="left"><strong>'.$def_modifier.'</strong></td>
-            <td class="table-info" valign="top" align="left">'.$type_meta.'</td>';
+            <td class="table-secondary" valign="top" align="left">'.$type_meta.'</td>';
       if ($type_meta=="smil") {
          eval($content);
          echo '
@@ -79,5 +85,10 @@ echo '<link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css">';/
             </td>
          </tr>
       </tbody>
-   </table>';
+   </table>
+   </div>';
+      echo '
+   <script type="text/javascript" src="lib/js/jquery.min.js"></script>
+   <script type="text/javascript" src="lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+   <script type="text/javascript" src="lib/js/npds_adapt.js"></script>';
 ?>
