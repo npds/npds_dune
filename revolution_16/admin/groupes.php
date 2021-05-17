2998ceeb70ff7883860b0b9cf5665476a0f23d99
jeanpierre <jean.barbary@wanadoo.fr>
2021-05-17 10:54:13 +0200

    homogénéisation table pour création bd du blocnotes

--- revolution_16/admin/groupes.php
+++ revolution_16/admin/groupes.php
@@ -622,14 +622,14 @@ function note_create($groupe_id) {
    else {$type_engine=(int)substr(mysql_get_server_info(), 0, 1);}// a affiner
 
    $sql="CREATE TABLE IF NOT EXISTS ".$NPDS_Prefix."blocnotes (
-   bnid tinytext NOT NULL,
-   texte text,
+   bnid text COLLATE utf8mb4_unicode_ci NOT NULL,
+   texte text COLLATE utf8mb4_unicode_ci,
    PRIMARY KEY (bnid(32))
    )";
    if ($type_engine>=5)
-      $sql.=" ENGINE=MyISAM";
+      $sql.=" ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
    else
-      $sql.=" TYPE=MyISAM";
+      $sql.=" TYPE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
    sql_query($sql);
 
    sql_query("UPDATE ".$NPDS_Prefix."groupes SET groupe_blocnote = '1' WHERE groupe_id = '$groupe_id';");
--- revolution_16/modules/bloc-notes/install/blocnotes.sql
+++ revolution_16/modules/bloc-notes/install/blocnotes.sql
@@ -5,7 +5,7 @@
 /*                                                                      */
 /* BLOC-NOTES engine for NPDS - Philippe Brunier & Arnaud Latourrette   */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2019 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2021 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -15,10 +15,10 @@
 /* Attention le cas échéant au prefix de cette table si vous avez plusieurs NPDS dans la même DB */
 
 CREATE TABLE blocnotes (
-  bnid tinytext NOT NULL,
-  texte text,
+  bnid text COLLATE utf8mb4_unicode_ci NOT NULL,
+  texte text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY  (bnid(32))
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO metalang VALUES ('!blocnote!', 'function MM_blocnote($arg) {\r\n      global $REQUEST_URI;\r\n      if (!stristr($REQUEST_URI,"admin.php")) {\r\n         return(@oneblock($arg,"RB"));\r\n      } else {\r\n         return("");\r\n      }\r\n}', 'meta', '-', NULL, '[french]Fabrique un blocnote contextuel en lieu et place du meta-mot / syntaxe : !blocnote!ID - ID = Id du bloc de droite dans le gestionnaire de bloc de NPDS[/french]', '0');
 ?>
