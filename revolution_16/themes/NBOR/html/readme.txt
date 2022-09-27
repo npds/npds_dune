924b0e91871ead571efc9df08fd8b6fad433dfb1
jeanpierre <jean.barbary@wanadoo.fr>
2022-09-27 19:08:19 +0200

    theme dynamique : simplification dossiers thèmes, consolidation
    - suppression des fichiers de template identiques à ceux fourni par themes/default/html
    - suppression des modèles forum-catX et forum-adv
    - rajout dans les thèmes d'un readme.txt avec explication sommaire
    - code  : complément pour le switch des templates edito defaut/theme
    - userpopover() défaillant sous certain window ... en cours de stabilisation

---
+++ revolution_16/themes/NBOR/html/readme.txt
@@ -0,0 +1,49 @@
+## Ce dossier peut contenir les modèles html de mise en page suivants qui remplaceront/compléteront ceux fournis par le dossier themes/default/html/
+##
+
+- bloc.html
+  # mise en page bloc(s) - non utilisé si bloc-left.html et bloc-right.html #
+- bloc-left.html
+  # mise en page bloc(s) défini(s) comme bloc(s) de "gauche" #
+- bloc-right.html
+  # mise en page bloc(s) défini(s) comme bloc(s) de "droite" #
+- central.html
+  # remplace l'index du site (si le fichier central.php est disponible à la racine du dossier de votre thème)
+- detail-news.html
+  # mise en page article #
+- editorial.html
+  # mise en page du pieds des pages de votre site #
+- footer.html
+  # mise en page du pieds des pages de votre site #
+- forum.html
+  # mise en page de l'index du forum #
+- forum-cat1.html
+  # mise en page de la catégorie de forum identifiée par la terminaison numérique du nom de fichier et création d'un lien dans l'index du forum menant à cette catégorie. On peut donc avoir un fichier par catégorie ... #
+- header.html
+  # mise en page de l'entête des pages de votre site #
+- index-news.html
+  # mise en page de l'index des articles #
+- forum-adv.html
+  # mise en page de l'index du forum -  ! attention : remplace le template forum.html #
+- topics.html 
+  # mise en page des sujets #
+- top.html
+  # (mise en page du top(X)) #
+- header_landing.html 
+  # mise en page de l'entête de la page de démarrage de votre site #
+
+- modules
+   - f-manager
+      - f-manager-banque.html
+        # mise en page du gestionnaire de fichier #
+      - f-manager-cmd.html
+        # mise en page du gestionnaire de fichier #
+      - f-manager.html
+        # mise en page du gestionnaire de fichier #
+      - foot.html
+        # mise en page du pied de page du gestionnaire de fichier #
+      - head.html
+        # mise en page de l'entête du gestionnaire de fichier #
+      - pic-editeur.html
+      - pic-manager.html
+        # mise en page du gestionnaire d'images #
--- revolution_16/themes/themes-dynamic/theme.php
+++ revolution_16/themes/themes-dynamic/theme.php
@@ -4,13 +4,14 @@
 /* ===========================                                          */
 /*                                                                      */
 /* DYNAMIC THEME engine for NPDS                                        */
-/* NPDS Copyright (c) 2002-2021 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2022 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 global $meta_glossaire;
+
 function local_var($Xcontent) {
    if (strstr($Xcontent,"!var!")) {
       $deb=strpos($Xcontent,"!var!",0)+5;
@@ -20,11 +21,14 @@ function local_var($Xcontent) {
       return ($H_var);
    }
 }
+
 function themeindex ($aid, $informant, $time, $title, $counter, $topic, $thetext, $notes, $morelink, $topicname, $topicimage, $topictext, $id) {
    global $tipath, $theme, $nuke_url;
    $inclusion=false;
-   if (file_exists("themes/".$theme."/html/index-news.html")) {$inclusion="themes/".$theme."/html/index-news.html";}
-   elseif (file_exists("themes/default/html/index-news.html")) {$inclusion="themes/default/html/index-news.html";}
+   if (file_exists("themes/".$theme."/html/index-news.html"))
+      $inclusion="themes/".$theme."/html/index-news.html";
+   elseif (file_exists("themes/default/html/index-news.html"))
+      $inclusion="themes/default/html/index-news.html";
    else {
       echo 'index-news.html manquant / not find !<br />';
       die();
@@ -82,12 +86,15 @@ function themeindex ($aid, $informant, $time, $title, $counter, $topic, $thetext
    );
    echo meta_lang(aff_langue(preg_replace(array_keys($npds_METALANG_words),array_values($npds_METALANG_words), $Xcontent)));
 }
+
 function themearticle ($aid, $informant, $time, $title, $thetext, $topic, $topicname, $topicimage, $topictext, $id, $previous_sid, $next_sid, $archive) {
    global $tipath, $theme, $nuke_url, $counter;
    global $boxtitle, $boxstuff, $short_user,$user;
    $inclusion=false;
-   if (file_exists("themes/".$theme."/html/detail-news.html")) {$inclusion="themes/".$theme."/html/detail-news.html";}
-   elseif (file_exists("themes/default/html/detail-news.html")) {$inclusion="themes/default/html/detail-news.html";}
+   if (file_exists("themes/".$theme."/html/detail-news.html"))
+      $inclusion="themes/".$theme."/html/detail-news.html";
+   elseif (file_exists("themes/default/html/detail-news.html"))
+      $inclusion="themes/default/html/detail-news.html";
    else {
       echo 'detail-news.html manquant / not find !<br />';
       die();
@@ -135,14 +142,19 @@ function themearticle ($aid, $informant, $time, $title, $thetext, $topic, $topic
    );
    echo meta_lang(aff_langue(preg_replace(array_keys($npds_METALANG_words),array_values($npds_METALANG_words), $Xcontent)));
 }
+
 function themesidebox($title, $content) {
    global $theme, $B_class_title, $B_class_content, $bloc_side, $htvar;
    $inclusion=false;
-   if (file_exists("themes/".$theme."/html/bloc-right.html") and ($bloc_side=="RIGHT")) {$inclusion='themes/'.$theme.'/html/bloc-right.html';}
-   if (file_exists("themes/".$theme."/html/bloc-left.html") and ($bloc_side=="LEFT")) {$inclusion='themes/'.$theme.'/html/bloc-left.html';}
+   if (file_exists("themes/".$theme."/html/bloc-right.html") and ($bloc_side=="RIGHT"))
+      $inclusion='themes/'.$theme.'/html/bloc-right.html';
+   if (file_exists("themes/".$theme."/html/bloc-left.html") and ($bloc_side=="LEFT")) 
+      $inclusion='themes/'.$theme.'/html/bloc-left.html';
    if (!$inclusion) {
-      if (file_exists("themes/".$theme."/html/bloc.html")) {$inclusion='themes/'.$theme.'/html/bloc.html';}
-      elseif (file_exists("themes/default/html/footer.html")) {$inclusion='themes/default/html/bloc.html';}
+      if (file_exists("themes/".$theme."/html/bloc.html"))
+         $inclusion='themes/'.$theme.'/html/bloc.html';
+      elseif (file_exists("themes/default/html/bloc.html"))
+         $inclusion='themes/default/html/bloc.html';
       else {
          echo 'bloc.html manquant / not find !<br />';
          die();
@@ -167,10 +179,18 @@ function themesidebox($title, $content) {
    echo '
             </div>';// modif ji fantôme block
 }
+
 function themedito($content) {
    global $theme;
    $inclusion=false;
-   if (file_exists("themes/".$theme."/html/editorial.html")) {$inclusion="themes/".$theme."/html/editorial.html";}
+   if (file_exists("themes/".$theme."/html/editorial.html"))
+      $inclusion="themes/".$theme."/html/editorial.html";
+      elseif (file_exists("themes/default/html/editorial.html"))
+         $inclusion="themes/default/html/editorial.html";
+      else {
+         echo 'editorial.html manquant / not find !<br />';
+         die();
+      }
    if ($inclusion) {
       ob_start();
       include($inclusion);
@@ -183,6 +203,7 @@ function themedito($content) {
    }
    return ($inclusion);
 }
+
 function userpopover($who,$dim) {
    global $short_user, $user, $NPDS_Prefix;
    $result=sql_query("SELECT uname FROM ".$NPDS_Prefix."users WHERE uname ='$who'");
@@ -223,26 +244,38 @@ function userpopover($who,$dim) {
    $useroutils = '';
    if($user or autorisation(-127)) {
       if ($temp_user['uid']!= 1 and $temp_user['uid']!='')
-         $useroutils .= '<a class="list-group-item text-primary text-center text-md-start" href="user.php?op=userinfo&amp;uname='.$temp_user['uname'].'" target="_blank" title="'.translate("Profil").'" ><i class="fa fa-2x fa-user align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Profil").'</span></a>';
+         $useroutils .= '<a class="dropdown-item text-center text-md-start" href="user.php?op=userinfo&amp;uname='.$temp_user['uname'].'" target="_blank" title="'.translate("Profil").'" ><i class="fa fa-lg fa-user align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Profil").'</span></a>';
       if ($temp_user['uid']!= 1 and $temp_user['uid']!='')
-         $useroutils .= '<a class="list-group-item text-primary text-center text-md-start" href="powerpack.php?op=instant_message&amp;to_userid='.urlencode($temp_user['uname']).'" title="'.translate("Envoyer un message interne").'" ><i class="far fa-2x fa-envelope align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Message").'</span></a>';
+         $useroutils .= '<a class="dropdown-item text-center text-md-start" href="powerpack.php?op=instant_message&amp;to_userid='.urlencode($temp_user['uname']).'" title="'.translate("Envoyer un message interne").'" ><i class="far fa-lg fa-envelope align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Message").'</span></a>';
       if ($temp_user['femail']!='')
-         $useroutils .= '<a class="list-group-item text-primary text-center text-md-start" href="mailto:'.anti_spam($temp_user['femail'],1).'" target="_blank" title="'.translate("Email").'" ><i class="fa fa-at fa-2x align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Email").'</span></a>';
+         $useroutils .= '<a class="dropdown-item  text-center text-md-start" href="mailto:'.anti_spam($temp_user['femail'],1).'" target="_blank" title="'.translate("Email").'" ><i class="fa fa-at fa-lg align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Email").'</span></a>';
       if ($temp_user['uid']!= 1 and array_key_exists($ch_lat, $posterdata_extend)) {
          if ($posterdata_extend[$ch_lat] !='')
-            $useroutils .= '<a class="list-group-item text-primary text-center text-md-start" href="modules.php?ModPath=geoloc&amp;ModStart=geoloc&op=u'.$temp_user['uid'].'" title="'.translate("Localisation").'" ><i class="fas fa-map-marker-alt fa-2x align-middle fa-fw">&nbsp;</i><span class="ms-2 d-none d-md-inline">'.translate("Localisation").'</span></a>';
+            $useroutils .= '<a class="dropdown-item text-center text-md-start" href="modules.php?ModPath=geoloc&amp;ModStart=geoloc&op=u'.$temp_user['uid'].'" title="'.translate("Localisation").'" ><i class="fas fa-map-marker-alt fa-lg align-middle fa-fw">&nbsp;</i><span class="ms-2 d-none d-md-inline">'.translate("Localisation").'</span></a>';
       }
    }
    if ($temp_user['url']!='')
-      $useroutils .= '<a class="list-group-item text-primary text-center text-md-start" href="'.$temp_user['url'].'" target="_blank" title="'.translate("Visiter ce site web").'"><i class="fas fa-external-link-alt fa-2x align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visiter ce site web").'</span></a>';
+      $useroutils .= '<a class="dropdown-item text-center text-md-start" href="'.$temp_user['url'].'" target="_blank" title="'.translate("Visiter ce site web").'"><i class="fas fa-external-link-alt fa-lg align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visiter ce site web").'</span></a>';
    if ($temp_user['mns'])
-       $useroutils .= '<a class="list-group-item text-primary text-center text-md-start" href="minisite.php?op='.$temp_user['uname'].'" target="_blank" target="_blank" title="'.translate("Visitez le minisite").'" ><i class="fa fa-2x fa-desktop align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visitez le minisite").'</span></a>';
+       $useroutils .= '<a class="dropdown-item text-center text-md-start" href="minisite.php?op='.$temp_user['uname'].'" target="_blank" target="_blank" title="'.translate("Visitez le minisite").'" ><i class="fa fa-lg fa-desktop align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visitez le minisite").'</span></a>';
 
    if (stristr($temp_user['user_avatar'],'users_private')) 
       $imgtmp=$temp_user['user_avatar'];
    else
       if ($ibid=theme_image('forum/avatar/'.$temp_user['user_avatar'])) {$imgtmp=$ibid;} else {$imgtmp='images/forum/avatar/'.$temp_user['user_avatar'];}
-   $userpop ='<a tabindex="0" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-html="true" data-bs-title="'.$temp_user['uname'].'" data-bs-content=\'<div class="list-group mb-3 text-center">'.$useroutils.'</div><div class="mx-auto text-center" style="max-width:170px;">'.$my_rs.'</div>\'></i><img data-bs-html="true" class="btn-outline-primary img-thumbnail img-fluid n-ava-'.$dim.' me-2" src="'.$imgtmp.'" alt="'.$temp_user['uname'].'" loading="lazy" /></a>';
+      $userpop ='<a tabindex="0" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-html="true" data-bs-title="'.$temp_user['uname'].'" data-bs-content=\'<div class="list-group mb-3 text-center">'.$useroutils.'</div><div class="mx-auto text-center" style="max-width:170px;">'.$my_rs.'</div>\'></i><img data-bs-html="true" class="btn-outline-primary img-thumbnail img-fluid n-ava-'.$dim.' me-2" src="'.$imgtmp.'" alt="'.$temp_user['uname'].'" loading="lazy" /></a>';
+
+
+/*
+ $userpop ='<div class="dropdown d-inline-block me-4 dropend">
+      <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+         <img class=" btn-outline-primary img-fluid n-ava-'.$dim.' me-0" src="'.$imgtmp.'" alt="'.$temp_user['uname'].'" />
+      </a>
+      <div class="dropdown-menu ">
+         <div class=" mb-3 text-center">'.$useroutils.'</div>
+     </div>
+   </div>';
+*/
 
    return $userpop;
    }
