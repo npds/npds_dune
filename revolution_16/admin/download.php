de95331712c6fdd460463cfeafaa1249aaf99610
jeanpierre <jean.barbary@wanadoo.fr>
2023-10-21 11:20:38 +0200

    interface + bug + code
    - stabilisation de la cohérence de l'interface (champ obligatoire) lors de la première utilisation (ie quand il n'y a pas de catégories définie)
    - ternary condition
    - correction affichage des icones des droits dans le tableau  (défaillants à cause de comparaison non adaptée au type de la variable)
    - rajout d'une option d'entête dans le select des catégories pour que cela entraine obligatoirement un onchange nécessaire au remplissage de l'input ce qui n'était pas le cas quand on avait une seule catégorie
    - rajout droits à tous par défaut
    - extension du nombre de car max pour durl (controle du formulaire)
    
    Co-Authored-By: Nicolas2 <129994189+Nicolas2dev@users.noreply.github.com>

--- revolution_16/admin/download.php
+++ revolution_16/admin/download.php
@@ -5,7 +5,7 @@
 /*                                                                      */
 /* Based on PhpNuke 4.x source code                                     */
 /*                                                                      */
-/* This version name NPDS Copyright (c) 2001-2022 by Philippe Brunier   */
+/* This version name NPDS Copyright (c) 2001-2023 by Philippe Brunier   */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -54,13 +54,13 @@ function droits($member) {
    echo '
    <div class="mb-3">
       <div class="form-check form-check-inline">';
-   if ($member==-127) $checked=' checked="checked"'; else $checked='';
+   $checked = ($member==-127) ? ' checked="checked"' : '' ;
    echo '
          <input type="radio" id="adm" name="privs" class="form-check-input" value="-127" '.$checked.' />
          <label class="form-check-label" for="adm">'.adm_translate("Administrateurs").'</label>
       </div>
       <div class="form-check form-check-inline">';
-   if ($member==-1) $checked=' checked="checked"'; else $checked='';
+   $checked = ($member==-1) ? ' checked="checked"' : '' ;
    echo '
          <input type="radio" id="ano" name="privs" class="form-check-input" value="-1" '.$checked.' />
          <label class="form-check-label" for="ano">'.adm_translate("Anonymes").'</label>
@@ -84,7 +84,7 @@ function droits($member) {
       </div>
    </div>';
    } else {
-      if ($member==0) $checked=' checked="checked"'; else $checked='';
+      $checked = ($member==0) ? ' checked="checked"' : '' ;
       echo '
          <input type="radio" id="mem" name="privs" class="form-check-input" value="1" />
          <label class="form-check-label" for="mem">'.adm_translate("Membres").'</label>
@@ -97,7 +97,7 @@ function droits($member) {
    <div class="mb-3 row">
       <label class="col-form-label col-sm-12" for="mpri">'.adm_translate("Groupes").'</label>
       <div class="col-sm-12">';
-         echo groupe($member).'
+      echo groupe($member).'
       </div>
    </div>';
    }
@@ -122,53 +122,53 @@ function DownloadAdmin() {
       '.aff_langue(stripslashes($dcategory)).'</h4>';
       echo '
    <div class="mb-3" id="cat_'.$pseudocatid.'" style="display:none;">
-   <table data-toggle="table" data-striped="true" data-search="true" data-show-toggle="true" data-show-columns="true" data-mobile-responsive="true" data-buttons-class="outline-secondary" data-icons-prefix="fa" data-icons="icons">
-      <thead>
-         <tr>
-            <th data-sortable="true" data-halign="center" data-align="right">'.adm_translate("ID").'</th>
-            <th data-sortable="true" data-halign="center" data-align="right">'.adm_translate("Compteur").'</th>
-            <th data-sortable="true" data-halign="center" data-align="center">Typ.</th>
-            <th data-halign="center" data-align="center">'.adm_translate("URL").'</th>
-            <th data-sortable="true" data-halign="center" >'.adm_translate("Nom de fichier").'</th>
-            <th data-halign="center" data-align="center">'.adm_translate("Version").'</th>
-            <th data-halign="center" data-align="right">'.adm_translate("Taille de fichier").'</th>
-            <th data-halign="center" >'.adm_translate("Date").'</th>
-            <th data-halign="center" data-align="center">'.adm_translate("Fonctions").'</th>
-         </tr>
-      </thead>
-      <tbody>';
-       $result = sql_query("SELECT did, dcounter, durl, dfilename, dfilesize, ddate, dver, perms FROM ".$NPDS_Prefix."downloads WHERE dcategory='".addslashes($dcategory)."' ORDER BY did ASC");
-       while(list($did, $dcounter, $durl, $dfilename, $dfilesize, $ddate, $dver, $dperm) = sql_fetch_row($result)) {
-          if ($dperm==0) $dperm='<span title="'.adm_translate("Anonymes").'<br />'.adm_translate("Membres").'<br />'.adm_translate("Administrateurs").'" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-html="true"><i class="far fa-user fa-lg"></i><i class="fas fa-user fa-lg"></i><i class="fa fa-user-cog fa-lg"></i></span>';
-          if ($dperm==1) $dperm='<span title="'.adm_translate("Membres").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fas fa-user fa-lg"></i></span>';
-          if ($dperm>=1) $dperm='<span title="'.adm_translate("Groupes").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fa fa-users fa-lg"></i></span>';
-          if ($dperm==-127) $dperm='<span title="'.adm_translate("Administrateurs").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fas fa-user-cog fa-lg"></i></span>';
-          if ($dperm==-1) $dperm='<span title="'.adm_translate("Anonymes").'"  data-bs-toggle="tooltip" data-bs-placement="right"><i class="far fa-user fa-lg"></i></span>';
-          echo '
-         <tr>
-            <td>'.$did.'</td>
-            <td>'.$dcounter.'</td>
-            <td>'.$dperm.'</td>
-            <td><a href="'.$durl.'" title="'.adm_translate("Téléchargements").'<br />'.$durl.'" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-html="true"><i class="fa fa-download fa-2x"></i></a></td>
-            <td>'.$dfilename.'</td>
-            <td><span class="small">'.$dver.'</span></td>
-            <td><span class="small">';
+      <table data-toggle="table" data-striped="true" data-search="true" data-show-toggle="true" data-show-columns="true" data-mobile-responsive="true" data-buttons-class="outline-secondary" data-icons-prefix="fa" data-icons="icons">
+         <thead>
+            <tr>
+               <th data-sortable="true" data-halign="center" data-align="right">'.adm_translate("ID").'</th>
+               <th data-sortable="true" data-halign="center" data-align="right">'.adm_translate("Compteur").'</th>
+               <th data-sortable="true" data-halign="center" data-align="center">Typ.</th>
+               <th data-halign="center" data-align="center">'.adm_translate("URL").'</th>
+               <th data-sortable="true" data-halign="center" >'.adm_translate("Nom de fichier").'</th>
+               <th data-halign="center" data-align="center">'.adm_translate("Version").'</th>
+               <th data-halign="center" data-align="right">'.adm_translate("Taille de fichier").'</th>
+               <th data-halign="center" >'.adm_translate("Date").'</th>
+               <th data-halign="center" data-align="center">'.adm_translate("Fonctions").'</th>
+            </tr>
+         </thead>
+         <tbody>';
+      $result = sql_query("SELECT did, dcounter, durl, dfilename, dfilesize, ddate, dver, perms FROM ".$NPDS_Prefix."downloads WHERE dcategory='".addslashes($dcategory)."' ORDER BY did ASC");
+      while(list($did, $dcounter, $durl, $dfilename, $dfilesize, $ddate, $dver, $dperm) = sql_fetch_row($result)) {
+         if ($dperm=='0') $dperm='<span title="'.adm_translate("Anonymes").'<br />'.adm_translate("Membres").'<br />'.adm_translate("Administrateurs").'" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-html="true"><i class="far fa-user fa-lg"></i><i class="fas fa-user fa-lg"></i><i class="fa fa-user-cog fa-lg"></i></span>';
+         else if ($dperm=='1') $dperm='<span title="'.adm_translate("Membres").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fas fa-user fa-lg"></i></span>';
+         else if ($dperm=='-127') $dperm='<span title="'.adm_translate("Administrateurs").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fas fa-user-cog fa-lg"></i></span>';
+         else if ($dperm=='-1') $dperm='<span title="'.adm_translate("Anonymes").'"  data-bs-toggle="tooltip" data-bs-placement="right"><i class="far fa-user fa-lg"></i></span>';
+         else $dperm='<span title="'.adm_translate("Groupes").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fa fa-users fa-lg"></i></span>';
+         echo '
+            <tr>
+               <td>'.$did.'</td>
+               <td>'.$dcounter.'</td>
+               <td>'.$dperm.'</td>
+               <td><a href="'.$durl.'" title="'.adm_translate("Téléchargements").'<br />'.$durl.'" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-html="true"><i class="fa fa-download fa-2x"></i></a></td>
+               <td>'.$dfilename.'</td>
+               <td><span class="small">'.$dver.'</span></td>
+               <td><span class="small">';
          $Fichier = new FileManagement;
          if ($dfilesize!=0)
             echo $Fichier->file_size_format($dfilesize, 1);
          else
             echo $Fichier->file_size_auto($durl, 2);
          echo '</span></td>
-            <td class="small">'.$ddate.'</td>
-            <td>
-               <a href="admin.php?op=DownloadEdit&amp;did='.$did.'" title="'.adm_translate("Editer").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fa fa-edit fa-lg"></i></a>
-               <a href="admin.php?op=DownloadDel&amp;did='.$did.'&amp;ok=0" title="'.adm_translate("Effacer").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fas fa-trash fa-lg text-danger ms-2"></i></a>
-            </td>
-            </tr>';
+               <td class="small">'.$ddate.'</td>
+               <td>
+                  <a href="admin.php?op=DownloadEdit&amp;did='.$did.'" title="'.adm_translate("Editer").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fa fa-edit fa-lg"></i></a>
+                  <a href="admin.php?op=DownloadDel&amp;did='.$did.'&amp;ok=0" title="'.adm_translate("Effacer").'" data-bs-toggle="tooltip" data-bs-placement="right"><i class="fas fa-trash fa-lg text-danger ms-2"></i></a>
+               </td>
+               </tr>';
        }
        echo '
-      </tbody>
-   </table>
+         </tbody>
+      </table>
    </div>';
    echo '
    <script type="text/javascript">
@@ -186,7 +186,7 @@ function DownloadAdmin() {
       <div class="mb-3 row">
          <label class="col-form-label col-sm-4" for="durl">'.adm_translate("Télécharger URL").'</label>
          <div class="col-sm-8">
-            <input class="form-control" type="text" id="durl" name="durl" maxlength="255" required="required" />
+            <input class="form-control" type="text" id="durl" name="durl" maxlength="320" required="required" />
    &nbsp;<a href="javascript:void(0);" onclick="window.open(\'admin.php?op=FileManagerDisplay\', \'wdir\', \'width=650, height=450, menubar=no, location=no, directories=no, status=no, copyhistory=no, toolbar=no, scrollbars=yes, resizable=yes\');">
    <span class="">['.adm_translate("Parcourir").']</span></a>
             <span class="help-block text-end" id="countcar_durl"></span>
@@ -235,14 +235,15 @@ function DownloadAdmin() {
       <div class="mb-3 row">
          <label class="col-form-label col-sm-4" for="dcategory">'.adm_translate("Catégorie").'</label>
          <div class="col-sm-8">
-            <input class="form-control" type="text" id="dcategory" name="dcategory" maxlength="250" />
+            <input class="form-control" type="text" id="dcategory" name="dcategory" maxlength="250" required="required"/>
             <span class="help-block text-end" id="countcar_dcategory"></span>
-            <select class="form-select" name="sdcategory">';
+            <select class="form-select" name="sdcategory" onchange="adminForm.dcategory.value=options[selectedIndex].value">
+               <option>'.adm_translate("Catégorie").'</option>';
    $result = sql_query("SELECT DISTINCT dcategory FROM ".$NPDS_Prefix."downloads ORDER BY dcategory");
    while (list($dcategory) = sql_fetch_row($result)) {
       $dcategory=stripslashes($dcategory);
       echo '
-               <option '.$sel.' value="'.$dcategory.'">'.aff_langue($dcategory).'</option>';
+               <option value="'.$dcategory.'">'.aff_langue($dcategory).'</option>';
     }
    echo '
             </select>
@@ -257,7 +258,7 @@ function DownloadAdmin() {
       '.aff_editeur('xtext','').'
       <fieldset>
          <legend>'.adm_translate("Droits").'</legend>';
-         droits('');
+         droits('0');
    echo '
       </fieldset>
       <input type="hidden" name="op" value="DownloadAdd" />
@@ -269,7 +270,7 @@ function DownloadAdmin() {
    </form>';
    $arg1='
          var formulid = ["downloadadd"];
-         inpandfieldlen("durl",255);
+         inpandfieldlen("durl",320);
          inpandfieldlen("dfilename",255);
          inpandfieldlen("dver",6);
          inpandfieldlen("dfilesize",31);
@@ -297,7 +298,7 @@ function DownloadEdit($did) {
       <div class="mb-3 row">
          <label class="col-form-label col-sm-4" for="durl">'.adm_translate("Télécharger URL").'</label>
          <div class="col-sm-8">
-            <input class="form-control" type="text" id="durl" name="durl" value="'.$durl.'" maxlength="255" required="required" />
+            <input class="form-control" type="text" id="durl" name="durl" value="'.$durl.'" maxlength="320" required="required" />
             <span class="help-block text-end" id="countcar_durl"></span>
          </div>
       </div>
@@ -338,13 +339,12 @@ function DownloadEdit($did) {
       <div class="mb-3 row">
          <label class="col-form-label col-sm-4" for="dcategory">'.adm_translate("Catégorie").'</label>
          <div class="col-sm-8">
-            <input class="form-control" type="text" id="dcategory" name="dcategory" value="'.stripslashes($dcategory).'" maxlength="250" />
+            <input class="form-control" type="text" id="dcategory" name="dcategory" value="'.stripslashes($dcategory).'" maxlength="250" required="required" />
             <span class="help-block text-end"><span id="countcar_dcategory"></span></span>
             <select class="form-select" name="sdcategory" onchange="adminForm.dcategory.value=options[selectedIndex].value">';
-   $result = sql_query("SELECT distinct dcategory FROM ".$NPDS_Prefix."downloads ORDER BY dcategory");
+   $result = sql_query("SELECT DISTINCT dcategory FROM ".$NPDS_Prefix."downloads ORDER BY dcategory");
    while (list($Xdcategory) = sql_fetch_row($result)) {
-      if ($Xdcategory==$dcategory) $sel='selected';
-      else $sel='';
+      $sel = $Xdcategory==$dcategory ? 'selected' : '';
       $Xdcategory=stripslashes($Xdcategory);
       echo '
                <option '.$sel.' value="'.$Xdcategory.'">'.aff_langue($Xdcategory).'</option>';
@@ -360,11 +360,11 @@ function DownloadEdit($did) {
          </div>
       </div>
       '.aff_editeur('xtext','');
-       echo '
+   echo '
       <fieldset>
          <legend>'.adm_translate("Droits").'</legend>';
        droits($privs);
-       echo '
+   echo '
       </fieldset>
       <div class="mb-3 row">
          <label class="col-form-label col-sm-4">'.adm_translate("Changer la date").'</label>
@@ -384,7 +384,7 @@ function DownloadEdit($did) {
    </form>';
    $arg1='
       var formulid = ["downloaded"];
-      inpandfieldlen("durl",255);
+      inpandfieldlen("durl",320);
       inpandfieldlen("dfilename",255);
       inpandfieldlen("dver",6);
       inpandfieldlen("dfilesize",31);
@@ -396,44 +396,38 @@ function DownloadEdit($did) {
 }
 
 function DownloadSave($did, $dcounter, $durl, $dfilename, $dfilesize, $dweb, $duser, $ddate, $dver, $dcategory, $sdcategory, $description, $privs, $Mprivs) {
-    global $NPDS_Prefix;
+   global $NPDS_Prefix;
    if ($privs==1) {
       if ($Mprivs!='')
          $privs = implode(',', $Mprivs);
    }
-    $sdcategory=addslashes($sdcategory);
-    if (!$dcategory)
-       $dcategory = $sdcategory;
-    else
-       $dcategory=addslashes($dcategory);
-    $description=addslashes($description);
-    if ($ddate=="yes") {
-       $time = date("Y-m-d");
-       sql_query("UPDATE ".$NPDS_Prefix."downloads SET dcounter='$dcounter', durl='$durl', dfilename='$dfilename', dfilesize='$dfilesize', ddate='$time', dweb='$dweb', duser='$duser', dver='$dver', dcategory='$dcategory', ddescription='$description', perms='$privs' WHERE did='$did'");
-    } else
-       sql_query("UPDATE ".$NPDS_Prefix."downloads SET dcounter='$dcounter', durl='$durl', dfilename='$dfilename', dfilesize='$dfilesize', dweb='$dweb', duser='$duser', dver='$dver', dcategory='$dcategory', ddescription='$description', perms='$privs' WHERE did='$did'");
-    Header("Location: admin.php?op=DownloadAdmin");
+   $sdcategory=addslashes($sdcategory);
+   $dcategory = (!$dcategory) ? $sdcategory : addslashes($dcategory) ;
+   $description=addslashes($description);
+   if ($ddate=="yes") {
+      $time = date("Y-m-d");
+      sql_query("UPDATE ".$NPDS_Prefix."downloads SET dcounter='$dcounter', durl='$durl', dfilename='$dfilename', dfilesize='$dfilesize', ddate='$time', dweb='$dweb', duser='$duser', dver='$dver', dcategory='$dcategory', ddescription='$description', perms='$privs' WHERE did='$did'");
+   } else
+      sql_query("UPDATE ".$NPDS_Prefix."downloads SET dcounter='$dcounter', durl='$durl', dfilename='$dfilename', dfilesize='$dfilesize', dweb='$dweb', duser='$duser', dver='$dver', dcategory='$dcategory', ddescription='$description', perms='$privs' WHERE did='$did'");
+   Header("Location: admin.php?op=DownloadAdmin");
 }
 
 function DownloadAdd($dcounter, $durl, $dfilename, $dfilesize, $dweb, $duser, $dver, $dcategory, $sdcategory, $description, $privs, $Mprivs) {
-    global $NPDS_Prefix;
+   global $NPDS_Prefix;
    if ($privs==1) {
-       if ($Mprivs>1 and $Mprivs<=127 and $Mprivs!='') $privs=$Mprivs;
-    }
-    $sdcategory=addslashes($sdcategory);
-    if (!$dcategory)
-       $dcategory = $sdcategory;
-    else
-       $dcategory=addslashes($dcategory);
-    $description=addslashes($description);
-    $time = date("Y-m-d");
-    if (($durl) and ($dfilename))
-       sql_query("INSERT INTO ".$NPDS_Prefix."downloads VALUES ('0', '0', '$durl', '$dfilename', '0', '$time', '$dweb', '$duser', '$dver', '$dcategory', '$description', '$privs')");
-    Header("Location: admin.php?op=DownloadAdmin");
+      if ($Mprivs>1 and $Mprivs<=127 and $Mprivs!='') $privs=$Mprivs;
+   }
+   $sdcategory=addslashes($sdcategory);
+   $dcategory = (!$dcategory) ? $sdcategory : addslashes($dcategory) ;
+   $description=addslashes($description);
+   $time = date("Y-m-d");
+   if (($durl) and ($dfilename))
+      sql_query("INSERT INTO ".$NPDS_Prefix."downloads VALUES ('0', '0', '$durl', '$dfilename', '0', '$time', '$dweb', '$duser', '$dver', '$dcategory', '$description', '$privs')");
+   Header("Location: admin.php?op=DownloadAdmin");
 }
 
 function DownloadDel($did, $ok=0) {
-global $NPDS_Prefix, $f_meta_nom;
+   global $NPDS_Prefix, $f_meta_nom;
    if ($ok==1) {
       sql_query("DELETE FROM ".$NPDS_Prefix."downloads WHERE did='$did'");
       Header("Location: admin.php?op=DownloadAdmin");
