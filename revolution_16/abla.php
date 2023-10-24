43630715d7c92f113aa2c509ef83b39589b348cc
jeanpierre <jean.barbary@wanadoo.fr>
2023-10-24 15:08:40 +0200

    path for help file ...

--- revolution_16/abla.php
+++ revolution_16/abla.php
@@ -5,7 +5,7 @@
 /*                                                                      */
 /* Based on PhpNuke 4.x source code                                     */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2020 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2023 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -24,7 +24,7 @@ $f_titre = translate("Tableau de bord");
 admindroits($aid,$f_meta_nom);
 //<== controle droit
 global $language;
-$hlpfile = '';
+$hlpfile = '/manuels/'.$language.'/abla.html';
 
 global $admin;
 if ($admin) {
