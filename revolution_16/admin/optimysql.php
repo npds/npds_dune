0b0f2e23f849b4a9cfa6b9f6092ea77a7bbf0033
jeanpierre <jean.barbary@wanadoo.fr>
2023-10-24 19:04:52 +0200

    chemin pour admin help file et requete inutile
    - requête de création inutile la table DOIT exister
    - global $dbname probablement inutile ici

--- revolution_16/admin/optimysql.php
+++ revolution_16/admin/optimysql.php
@@ -5,7 +5,7 @@
 /*                                                                      */
 /* M. PASCAL aKa EBH (plan.net@free.fr)                                 */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2022 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2023 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -19,15 +19,15 @@ $f_titre = adm_translate("Optimisation de la base de données").' : '.$dbname;
 //==> controle droit
 admindroits($aid,$f_meta_nom);
 //<== controle droit
+$hlpfile = 'manuels/'.$language.'/optimysql.html'; 
 
 $date_opt = date(adm_translate("dateforop"));
 $heure_opt = date("h:i a");
 include("header.php");
 GraphicAdmin($hlpfile);
-global $dbname;
 
-// Création de la table optimy (si nécessaire)
-$result = sql_query("CREATE TABLE IF NOT EXISTS ".$NPDS_Prefix."optimy (optid INT(11) NOT NULL auto_increment, optgain DECIMAL(10,3), optdate VARCHAR (11) DEFAULT '', opthour VARCHAR (8) DEFAULT '', optcount INT(11) DEFAULT '0', PRIMARY KEY (optid))");
+global $dbname; // non utile ?
+
 // Insertion de valeurs d'initialisation de la table (si nécessaire)
 $result = sql_query("SELECT optid FROM ".$NPDS_Prefix."optimy");
 list($idopt) = sql_fetch_row($result);
@@ -101,26 +101,26 @@ echo $last_opti;
 echo '
 '.adm_translate("A ce jour, vous avez effectué ").' '.$countopt.' optimisation(s) '.adm_translate(" et réalisé un gain global de ").' '.$gainopt.' Ko.</p>
 <table id="tad_opti" data-toggle="table" data-striped="true" data-show-toggle="true" data-mobile-responsive="true" data-icons="icons" data-icons-prefix="fa">
-<thead>
-    <tr>
-        <th data-sortable="true" data-halign="center" data-align="center">'.adm_translate('Table').'</th>
-        <th data-halign="center" data-align="center">'.adm_translate('Taille actuelle').'</th>
-        <th data-sortable="true" data-halign="center" data-align="center">'.adm_translate('Etat').'</th>
-        <th data-halign="center" date-align="center">'.adm_translate('Gain réalisable').'</th>
-    </tr>
-</thead>
-<tfoot>
-    <tr>
-        <td></td>
-        <td></td>
-        <td>'.adm_translate("Gain total réalisé").' : </td>
-        <td>'.$total_gain.' Ko</td>
-    </tr>
-</tfoot>
-<tbody>';
+   <thead>
+       <tr>
+           <th data-sortable="true" data-halign="center" data-align="center">'.adm_translate('Table').'</th>
+           <th data-halign="center" data-align="center">'.adm_translate('Taille actuelle').'</th>
+           <th data-sortable="true" data-halign="center" data-align="center">'.adm_translate('Etat').'</th>
+           <th data-halign="center" date-align="center">'.adm_translate('Gain réalisable').'</th>
+       </tr>
+   </thead>
+   <tfoot>
+       <tr>
+           <td></td>
+           <td></td>
+           <td>'.adm_translate("Gain total réalisé").' : </td>
+           <td>'.$total_gain.' Ko</td>
+       </tr>
+   </tfoot>
+   <tbody>';
 echo $li_tab_opti;
 echo '
-</tbody>
+   </tbody>
 </table>';
 adminfoot('','','','');
 global $aid; Ecr_Log('security', "OptiMySql() by AID : $aid", '');
