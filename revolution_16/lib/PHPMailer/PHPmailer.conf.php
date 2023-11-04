73ee42becc2de07a1f13454f60d30fb155baba3e
jeanpierre <jean.barbary@wanadoo.fr>
2023-11-04 12:12:10 +0100

    defaut conf PHPmailer

--- revolution_16/lib/PHPMailer/PHPmailer.conf.php
+++ revolution_16/lib/PHPMailer/PHPmailer.conf.php
@@ -5,7 +5,7 @@
 #
 # This version name NPDS Copyright (c) 2001-2023 by Philippe Brunier
 #
-# This module is to configure Footer of Email send By NPDS
+# This file is to configure PHPMailer to send email from NPDS portal
 #
 # This program is free software. You can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,18 +14,18 @@
 
 # Configurer le serveur SMTP
 $smtp_host = "";
+# Port TCP, utilisez 587 si vous avez activé le chiffrement TLS
+$smtp_port = "";
 # Activer l'authentification SMTP
-$smtp_auth = 1;
+$smtp_auth = 0;
 # Nom d'utilisateur SMTP
 $smtp_username = "";
 # Mot de passe SMTP
 $smtp_password = "";
 # Activer le chiffrement TLS
-$smtp_secure = 1;
+$smtp_secure = 0;
 # Type du chiffrement TLS
 $smtp_crypt = "tls";
-# Port TCP, utilisez 587 si vous avez activé le chiffrement TLS
-$smtp_port = 587;
 # DKIM 1 pour celui du dns 2 pour une génération automatique
 $dkim_auto = 1;
 ?>
