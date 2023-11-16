e75285adb8563b695d2ebb97c729ace0d49f3b4e
jeanpierre <jean.barbary@wanadoo.fr>
2023-11-16 11:11:00 +0100

    ternary + year

--- revolution_16/modules.php
+++ revolution_16/modules.php
@@ -5,7 +5,7 @@
 /*                                                                      */
 /* Based on PhpNuke 4.x source code                                     */
 /*                                                                      */
-/* NPDS Copyright (c) 2002-2021 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2023 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -16,16 +16,14 @@ include ("grab_globals.php");
 function Access_Error () {
   include("admin/die.php");
 }
+
 function filtre_module($strtmp) {
    if (strstr($strtmp,'..') || stristr($strtmp,'script') || stristr($strtmp,'cookie') || stristr($strtmp,'iframe') || stristr($strtmp,'applet') || stristr($strtmp,'object'))
       Access_Error();
-   else {
-      if ($strtmp!='')
-         return (true);
-      else
-         return (false);
-   }
+   else
+      return $strtmp!='' ? true : false ;
 }
+
 if (filtre_module($ModPath) and filtre_module($ModStart)) {
    if (!function_exists("Mysql_Connexion"))
       include ("mainfile.php");
