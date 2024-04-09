8be2de363e7a250dae0ebff148b0e6064c15ca2c
jeanpierre <jean.barbary@wanadoo.fr>
2024-04-09 18:31:44 +0200

    update install auto

--- revolution_16/install/libraries/graphIZm.php
+++ revolution_16/install/libraries/graphIZm.php
@@ -44,7 +44,7 @@ function entete() {
       <meta name="robots" content="none" />
       <meta name="generator" content="NPDS IZ-Xinstall" />
       <link rel="stylesheet" href="lib/font-awesome/css/all.min.css" />
-      <link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="themes/_skins/cosmo/bootstrap.min.css" />
       <link rel="stylesheet" href="lib/formvalidation/dist/css/formValidation.min.css">
       <link rel="stylesheet" href="themes/npds-boost_sk/style/style.css">
       <link rel="shortcut icon" href="install/images/favicon.ico" type="image/x-icon">
@@ -53,32 +53,33 @@ function entete() {
       <script type="text/javascript" src="lib/js/npds_adapt.js"></script>
    </head>
    <body>
-      <div class="container-fluid p-0">
-         <div class="p-2 mb-4 bg-light">
-            <div class="row">
-               <div class="col-sm-2 d-none d-md-inline-block"><img class="img-fluid" src="install/images/header.png" alt="NPDS logo" /></div>
-               <div id="logo_header" class="col my-auto ps-3 ps-md-0">
-                  <h1 class="display-4">NPDS<br /><small class="text-muted">'.ins_translate("Installation automatique").' <em> '.NEW_VERSION.'</em></small></h1>
-               </div>
-               <div class="col-sm-3 text-end small my-auto">
-                  <ul class="list-group list-group-flush">
-                     <li class="bg-transparent border-0 p-0 list-group-item">'.$Version_Sub.' '.NEW_VERSION.'</li>
-                     <li class="bg-transparent border-0 p-0 list-group-item">Php '.$phpver.'</li>
-                     <li class="bg-transparent border-0 p-0 list-group-item">Sql '.$sqlver.'</li>
-                  </ul>
-               </div>
+      <div class="p-2 mb-4 bg-light">
+         <div class="row">
+            <div class="col-sm-2 d-none d-md-inline-block"><img class="img-fluid" src="install/images/header.png" alt="NPDS logo" /></div>
+            <div id="logo_header" class="col my-auto ps-3 ps-md-0">
+               <h1 class="display-4">NPDS<br /><small class="text-muted">'.ins_translate("Installation automatique").' <em> '.NEW_VERSION.'</em></small></h1>
             </div>
-         </div>';
+            <div class="col-sm-3 text-end small my-auto">
+               <ul class="list-group list-group-flush">
+                  <li class="bg-transparent border-0 p-0 list-group-item">'.$Version_Sub.' '.NEW_VERSION.'</li>
+                  <li class="bg-transparent border-0 p-0 list-group-item">Php '.$phpver.'</li>
+                  <li class="bg-transparent border-0 p-0 list-group-item">Sql '.$sqlver.'</li>
+               </ul>
+            </div>
+         </div>
+      </div>
+      <div class="container-lg p-0">';
 }
 
 function pied_depage($etat) {
    global $stage;
    echo '
-         <footer class="d-flex align-items-center bg-light p-4 mt-4">
-            <div class="fw-semibold"><a href="http://www.npds.org" target="_blank">NPDS</a> IZ-Xinstall v.1.3</div>
-            <div class="spinner-border ms-auto text-'.$etat.'" role="status" aria-hidden="true"></div>
-         </footer>
+         </div>
       </div>
+      <footer class="d-flex align-items-center bg-light p-4 mt-4">
+         <div class=""><a href="http://www.npds.org" target="_blank">NPDS</a> IZ-Xinstall v.1.3</div>
+         <div class="spinner-border ms-auto text-'.$etat.'" role="status" aria-hidden="true"></div>
+      </footer>
    </body>
 </html>';
    exit();
@@ -95,19 +96,19 @@ function menu() {
    global $menu, $langue, $colorst1, $colorst2, $colorst3, $colorst4, $colorst5, $colorst6, $colorst7, $colorst8, $colorst9, $colorst10;
    $menu='';
    $menu.= '
-         <div class="row px-3">
+         <div class="row px-3 g-3">
             <div class="col-md-3">
-               <ul class="list-group mb-3">
-                  <li class="list-group-item list-group-item'.$colorst1.'">'.ins_translate('Langue').'</li>
-                  <li class="list-group-item list-group-item'.$colorst2.'">'.ins_translate('Bienvenue').'</li>
-                  <li class="list-group-item list-group-item'.$colorst3.'">'.ins_translate('Licence').'</li>
-                  <li class="list-group-item list-group-item'.$colorst4.'">'.ins_translate('Vérification des fichiers').'</li>
-                  <li class="list-group-item list-group-item'.$colorst5.'">'.ins_translate('Paramètres de connexion').'</li>
-                  <li class="list-group-item list-group-item'.$colorst6.'">'.ins_translate('Autres paramètres').'</li>
-                  <li class="list-group-item list-group-item'.$colorst7.'">'.ins_translate('Base de données').'</li>
-                  <li class="list-group-item list-group-item'.$colorst8.'">'.ins_translate('Compte Admin').'</li>
-                  <li class="list-group-item list-group-item'.$colorst9.'">'.ins_translate('Module UPload').'</li>
-                  <li class="list-group-item list-group-item'.$colorst10.'">'.ins_translate('Fin').'</li>
+               <ul class="list-group mb-3 small text-md-end">
+                  <li class="list-group-item list-group-item'.$colorst1.' px-2 py-1 border-0">'.ins_translate('Langue').'</li>
+                  <li class="list-group-item list-group-item'.$colorst2.' px-2 py-1 border-0">'.ins_translate('Bienvenue').'</li>
+                  <li class="list-group-item list-group-item'.$colorst3.' px-2 py-1 border-0">'.ins_translate('Licence').'</li>
+                  <li class="list-group-item list-group-item'.$colorst4.' px-2 py-1 border-0">'.ins_translate('Vérification des fichiers').'</li>
+                  <li class="list-group-item list-group-item'.$colorst5.' px-2 py-1 border-0">'.ins_translate('Paramètres de connexion').'</li>
+                  <li class="list-group-item list-group-item'.$colorst6.' px-2 py-1 border-0">'.ins_translate('Autres paramètres').'</li>
+                  <li class="list-group-item list-group-item'.$colorst7.' px-2 py-1 border-0">'.ins_translate('Base de données').'</li>
+                  <li class="list-group-item list-group-item'.$colorst8.' px-2 py-1 border-0">'.ins_translate('Compte Admin').'</li>
+                  <li class="list-group-item list-group-item'.$colorst9.' px-2 py-1 border-0">'.ins_translate('Module UPload').'</li>
+                  <li class="list-group-item list-group-item'.$colorst10.' px-2 py-1 border-0">'.ins_translate('Fin').'</li>
                </ul>
             </div>
             <div class="col-md-9">';
