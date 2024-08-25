487193f0722d34e2854fa905e1e4767da413696e
jeanpierre <jean.barbary@wanadoo.fr>
2024-08-25 08:46:02 +0200

    total pages vues erroné
    https: //labo.infocapagde.com/viewtopic.php?topic=1822&forum=18
    Co-Authored-By: Nicolas2 <158532428+Nicolas2-dev@users.noreply.github.com>

--- revolution_16/mainfile.php
+++ revolution_16/mainfile.php
@@ -488,22 +488,26 @@ function req_stat() {
    // Les membres
    $result = sql_query("SELECT uid FROM ".$NPDS_Prefix."users");
    $xtab[0] = $result ? (sql_num_rows($result)-1) : '0' ;
+   if ($result) sql_free_result($result);
    // Les Nouvelles (News)
    $result = sql_query("SELECT sid FROM ".$NPDS_Prefix."stories");
    $xtab[1] = $result ? sql_num_rows($result) : '0' ;
+   if ($result) sql_free_result($result);
    // Les Critiques (Reviews))
    $result = sql_query("SELECT id FROM ".$NPDS_Prefix."reviews");
    $xtab[2] = $result ? sql_num_rows($result) : '0' ;
+   if ($result) sql_free_result($result);
    // Les Forums
    $result = sql_query("SELECT forum_id FROM ".$NPDS_Prefix."forums");
    $xtab[3] = $result ? sql_num_rows($result) : '0' ;
+   if ($result) sql_free_result($result);
    // Les Sujets (topics)
    $result = sql_query("SELECT topicid FROM ".$NPDS_Prefix."topics");
    $xtab[4] = $result ? sql_num_rows($result) : '0' ;
+   if ($result) sql_free_result($result);
    // Nombre de pages vues
    $result = sql_query("SELECT count FROM ".$NPDS_Prefix."counter WHERE type='total'");
-   if ($result) {list($totalz)=sql_fetch_row($result);}
-   $totalz++;
+   list($totalz)=sql_fetch_row($result);
    $xtab[5]=$totalz++;
    sql_free_result($result);
    return($xtab);
