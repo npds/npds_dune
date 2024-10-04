2d02d430b9f36aaf33eb8469e570273c048c8864
jeanpierre <jean.barbary@wanadoo.fr>
2024-10-04 17:23:48 +0200

    ajout trad pour interface + remise par ordre alphabétique des occurences

--- revolution_16/modules/f-manager/lang/f-manager-german.php
+++ revolution_16/modules/f-manager/lang/f-manager-german.php
@@ -11,52 +11,54 @@
 /************************************************************************/
 
 function fma_translate($phrase) {
- switch ($phrase) {
-   case "Fichier interdit" : $tmp = "Verbotene Datei"; break;
-   case "Type de fichier interdit" : $tmp = "Gebetteter Dateityp"; break;
-   case "Ok" : $tmp = "Ok"; break;
-   case "Renommer" : $tmp = "Umbenennen"; break;
-   case "Supprimer" : $tmp = "Entfernen"; break;
-   case "Chmoder" : $tmp = "Chmod"; break;
-   case "Editer" : $tmp = "Bearbeiten"; break;
-   case "Déplacer" : $tmp = "Bewegen"; break;
-   case "Copier" : $tmp = "Kopie"; break;
-   case "Déplacer / Copier" : $tmp = "Bewegen / Kopie"; break;
-   case "Renommer un fichier" : $tmp = "Umbenennen einer Datei"; break;
-   case "Déplacer / Copier un fichier" : $tmp = "Bewegen / Kopie einer Datei"; break;
-   case "Renommer un répertoire" : $tmp = "Umbenennen eines Verzeichnisses"; break;
-   case "Supprimer un fichier" : $tmp = "Löschen einer Datei"; break;
-   case "Supprimer un répertoire" : $tmp = "Löschen ein Verzeichnis"; break;
-   case "Confirmez-vous la suppression de" : $tmp = "Bestätigen Sie die Entfernung von"; break;
-   case "Changer les droits d'un fichier" : $tmp = "Ändern der Rechte einer Datei"; break;
-   case "Changer les droits d'un répertoire" : $tmp = "Ändern Sie die Rechte eines Verzeichnisses"; break;
-   case "Editer un fichier" : $tmp = "Bearbeiten einer Datei"; break;
-   case "Impossible d'appliquer le chmod" : $tmp = "Kann nicht gelten die chmod"; break;
-   case "Impossible de renommer" : $tmp = "Kann nicht umbenannt werden"; break;
-   case "Impossible de déplacer" : $tmp = "Kann sich nicht bewegen"; break;
-   case "Impossible de créer" : $tmp = "Kann nicht erstellt werden"; break;
-   case "Impossible de supprimer" : $tmp = "Kann nicht gelöscht werden"; break;
-   case "Impossible de copier" : $tmp = "Kann nicht kopiert werden"; break;
-   case "Le fichier n'existe pas" : $tmp = "Die Datei existiert nicht"; break;
-   case "existe déjà" : $tmp = "bereits vorhanden"; break;
-   case "Rafraîchir" : $tmp = "Aktualisierung"; break;
-   case "Extensions autorisées : " : $tmp = "Autorisierter Dateien Typ : "; break;
-   case "Go" : $tmp = "Go"; break;
-   case "Mo" : $tmp = "Mo"; break;
-   case "Ko" : $tmp = "Ko"; break;
-   case "Copie de " : $tmp = "Kopieren von "; break;
-   case "Taille maximum d'un fichier : " : $tmp = "Maximale Dateigröße : "; break;
-   case "Images manager" : $tmp = "Bilder Manager"; break;
-   case "Autoriser Pic-Manager" : $tmp = "Ermöglichen Bilder Manager"; break;
-   case "Taille maximum (pixel) de l'imagette" : $tmp = "Maximale Größe (pixel) für thumb"; break;
-   case "Cliquer ici pour charger le fichier dans le player" : $tmp = "Klicken Sie hier, um die Datei in den Player zu laden"; break;
-   case "Temps de cache (en seconde) des imagettes" : $tmp = "Dauer der Cache (in Sekunden) für thumbs"; break;
-   case "F-Manager": $tmp="F-Manager"; break;
-   case "Gestionnaire de fichiers": $tmp="Dateimanager"; break;
-   case "Sélectionner votre fichier": $tmp="Wählen Sie Ihre Datei aus"; break;
+   switch ($phrase) {
+      case "Autoriser Pic-Manager" : $tmp = "Ermöglichen Bilder Manager"; break;
+      case "Changer les droits d'un fichier" : $tmp = "Ändern der Rechte einer Datei"; break;
+      case "Changer les droits d'un répertoire" : $tmp = "Ändern Sie die Rechte eines Verzeichnisses"; break;
+      case "Chmoder" : $tmp = "Chmod"; break;
+      case "Cliquer ici pour charger le fichier dans le player" : $tmp = "Klicken Sie hier, um die Datei in den Player zu laden"; break;
+      case "Confirmez-vous la suppression de" : $tmp = "Bestätigen Sie die Entfernung von"; break;
+      case "Copie de " : $tmp = "Kopieren von "; break;
+      case "Copier" : $tmp = "Kopie"; break;
+      case "Déplacer / Copier un fichier" : $tmp = "Bewegen / Kopie einer Datei"; break;
+      case "Déplacer / Copier" : $tmp = "Bewegen / Kopie"; break;
+      case "Déplacer" : $tmp = "Bewegen"; break;
+      case "Ecouter" : $tmp = "Hören"; break;
+      case "Editer un fichier" : $tmp = "Bearbeiten einer Datei"; break;
+      case "Editer" : $tmp = "Bearbeiten"; break;
+      case "existe déjà" : $tmp = "bereits vorhanden"; break;
+      case "Extensions autorisées : " : $tmp = "Autorisierter Dateien Typ : "; break;
+      case "F-Manager": $tmp="F-Manager"; break;
+      case "Fichier interdit" : $tmp = "Verbotene Datei"; break;
+      case "Gestionnaire de fichiers": $tmp="Dateimanager"; break;
+      case "Go" : $tmp = "Go"; break;
+      case "Images manager" : $tmp = "Bilder Manager"; break;
+      case "Impossible d'appliquer le chmod" : $tmp = "Kann nicht gelten die chmod"; break;
+      case "Impossible de copier" : $tmp = "Kann nicht kopiert werden"; break;
+      case "Impossible de créer" : $tmp = "Kann nicht erstellt werden"; break;
+      case "Impossible de déplacer" : $tmp = "Kann sich nicht bewegen"; break;
+      case "Impossible de renommer" : $tmp = "Kann nicht umbenannt werden"; break;
+      case "Impossible de supprimer" : $tmp = "Kann nicht gelöscht werden"; break;
+      case "Ko" : $tmp = "Ko"; break;
+      case "Le fichier n'existe pas" : $tmp = "Die Datei existiert nicht"; break;
+      case "Mo" : $tmp = "Mo"; break;
+      case "Ok" : $tmp = "Ok"; break;
+      case "Rafraîchir" : $tmp = "Aktualisierung"; break;
+      case "Renommer un fichier" : $tmp = "Umbenennen einer Datei"; break;
+      case "Renommer un répertoire" : $tmp = "Umbenennen eines Verzeichnisses"; break;
+      case "Renommer" : $tmp = "Umbenennen"; break;
+      case "Sélectionner votre fichier": $tmp="Wählen Sie Ihre Datei aus"; break;
+      case "Supprimer un fichier" : $tmp = "Löschen einer Datei"; break;
+      case "Supprimer un répertoire" : $tmp = "Löschen ein Verzeichnis"; break;
+      case "Supprimer" : $tmp = "Entfernen"; break;
+      case "Taille maximum (pixel) de l'imagette" : $tmp = "Maximale Größe (pixel) für thumb"; break;
+      case "Taille maximum d'un fichier : " : $tmp = "Maximale Dateigröße : "; break;
+      case "Temps de cache (en seconde) des imagettes" : $tmp = "Dauer der Cache (in Sekunden) für thumbs"; break;
+      case "Type de fichier interdit" : $tmp = "Gebetteter Dateityp"; break;
+      case "Vous n'êtes pas autorisé à utiliser le gestionnaire de média. SVP contacter l'administrateur." : $tmp = "Sie sind nicht berechtigt, den Medienmanager zu verwenden. Bitte wenden Sie sich an den Administrator."; break;
 
-   default: $tmp = "Es gibt keine übersetzung [** $phrase **]"; break;
- }
+      default: $tmp = "Es gibt keine übersetzung [** $phrase **]"; break;
+   }
  return (htmlentities($tmp,ENT_QUOTES|ENT_SUBSTITUTE|ENT_HTML401,cur_charset));
 }
 ?>
--- revolution_16/modules/f-manager/lang/f-manager-spanish.php
+++ revolution_16/modules/f-manager/lang/f-manager-spanish.php
@@ -11,52 +11,54 @@
 /************************************************************************/
 
 function fma_translate($phrase) {
- switch ($phrase) {
-   case "Fichier interdit" : $tmp = "Fichero no permitido"; break;
-   case "Type de fichier interdit" : $tmp = "Tipo de fichero no permitido"; break;
-   case "Ok" : $tmp = "Ok"; break;
-   case "Renommer" : $tmp = "Cambiar"; break;
-   case "Supprimer" : $tmp = "Eliminar"; break;
-   case "Chmoder" : $tmp = "Chmod"; break;
-   case "Editer" : $tmp = "Editar"; break;
-   case "Déplacer" : $tmp = "Mover"; break;
-   case "Copier" : $tmp = "Copiar"; break;
-   case "Déplacer / Copier" : $tmp = "Mover / Copiar"; break;
-   case "Renommer un fichier" : $tmp = "Cambiar un fichero"; break;
-   case "Déplacer / Copier un fichier" : $tmp = "Mover / Copiar un fichero"; break;
-   case "Renommer un répertoire" : $tmp = "Cambiar el directorio"; break;
-   case "Supprimer un fichier" : $tmp = "Eliminar el fichero"; break;
-   case "Supprimer un répertoire" : $tmp = "Borrar el directorio"; break;
-   case "Confirmez-vous la suppression de" : $tmp = "Est· seguro que desea eliminar"; break;
-   case "Changer les droits d'un fichier" : $tmp = "Chmod de fichero"; break;
-   case "Changer les droits d'un répertoire" : $tmp = "Chmod directorio"; break;
-   case "Editer un fichier" : $tmp = "Editar el fichero"; break;
-   case "Impossible d'appliquer le chmod" : $tmp = "No se puede chmod"; break;
-   case "Impossible de renommer" : $tmp = "No se puede cambiar"; break;
-   case "Impossible de déplacer" : $tmp = "No se puede mover"; break;
-   case "Impossible de créer" : $tmp = "No se puede crear"; break;
-   case "Impossible de supprimer" : $tmp = "No se puede eliminar"; break;
-   case "Impossible de copier" : $tmp = "No se puede copiar"; break;
-   case "Le fichier n'existe pas" : $tmp = "El fichero no existe"; break;
-   case "existe déjà" : $tmp = "ya existen"; break;
-   case "Rafraîchir" : $tmp = "Actualización"; break;
-   case "Extensions autorisées : " : $tmp = "Tipo de fichero autorizado : "; break;
-   case "Go" : $tmp = "Go"; break;
-   case "Mo" : $tmp = "Mo"; break;
-   case "Ko" : $tmp = "Ko"; break;
-   case "Copie de " : $tmp = "Copia de "; break;
-   case "Taille maximum d'un fichier : " : $tmp = "Tamaño máximo de fichero : "; break;
-   case "Images manager" : $tmp = "Gerente de fotos"; break;
-   case "Autoriser Pic-Manager" : $tmp = "Permitir gerente de fotos"; break;
-   case "Taille maximum (pixel) de l'imagette" : $tmp = "El tamaño máximo (pixel) para thumb"; break;
-   case "Cliquer ici pour charger le fichier dans le player" : $tmp = "Haga clic aquí para cargar el fichero en el reproductor"; break;
-   case "Temps de cache (en seconde) des imagettes" : $tmp = "Duración de la caché (en segundos) para thumbs"; break;
-   case "F-Manager": $tmp="F-Manager"; break;
-   case "Gestionnaire de fichiers": $tmp="Gestor de archivos"; break;
-   case "Sélectionner votre fichier": $tmp="Selecciona tu archivo"; break;
+   switch ($phrase) {
+      case "Autoriser Pic-Manager" : $tmp = "Permitir gerente de fotos"; break;
+      case "Changer les droits d'un fichier" : $tmp = "Chmod de fichero"; break;
+      case "Changer les droits d'un répertoire" : $tmp = "Chmod directorio"; break;
+      case "Chmoder" : $tmp = "Chmod"; break;
+      case "Cliquer ici pour charger le fichier dans le player" : $tmp = "Haga clic aquí para cargar el fichero en el reproductor"; break;
+      case "Confirmez-vous la suppression de" : $tmp = "Est· seguro que desea eliminar"; break;
+      case "Copie de " : $tmp = "Copia de "; break;
+      case "Copier" : $tmp = "Copiar"; break;
+      case "Déplacer / Copier un fichier" : $tmp = "Mover / Copiar un fichero"; break;
+      case "Déplacer / Copier" : $tmp = "Mover / Copiar"; break;
+      case "Déplacer" : $tmp = "Mover"; break;
+      case "Ecouter" : $tmp = "Escuchar"; break;
+      case "Editer un fichier" : $tmp = "Editar el fichero"; break;
+      case "Editer" : $tmp = "Editar"; break;
+      case "existe déjà" : $tmp = "ya existen"; break;
+      case "Extensions autorisées : " : $tmp = "Tipo de fichero autorizado : "; break;
+      case "F-Manager": $tmp="F-Manager"; break;
+      case "Fichier interdit" : $tmp = "Fichero no permitido"; break;
+      case "Gestionnaire de fichiers": $tmp="Gestor de archivos"; break;
+      case "Go" : $tmp = "Go"; break;
+      case "Images manager" : $tmp = "Gerente de fotos"; break;
+      case "Impossible d'appliquer le chmod" : $tmp = "No se puede chmod"; break;
+      case "Impossible de copier" : $tmp = "No se puede copiar"; break;
+      case "Impossible de créer" : $tmp = "No se puede crear"; break;
+      case "Impossible de déplacer" : $tmp = "No se puede mover"; break;
+      case "Impossible de renommer" : $tmp = "No se puede cambiar"; break;
+      case "Impossible de supprimer" : $tmp = "No se puede eliminar"; break;
+      case "Ko" : $tmp = "Ko"; break;
+      case "Le fichier n'existe pas" : $tmp = "El fichero no existe"; break;
+      case "Mo" : $tmp = "Mo"; break;
+      case "Ok" : $tmp = "Ok"; break;
+      case "Rafraîchir" : $tmp = "Actualización"; break;
+      case "Renommer un fichier" : $tmp = "Cambiar un fichero"; break;
+      case "Renommer un répertoire" : $tmp = "Cambiar el directorio"; break;
+      case "Renommer" : $tmp = "Cambiar"; break;
+      case "Sélectionner votre fichier": $tmp="Selecciona tu archivo"; break;
+      case "Supprimer un fichier" : $tmp = "Eliminar el fichero"; break;
+      case "Supprimer un répertoire" : $tmp = "Borrar el directorio"; break;
+      case "Supprimer" : $tmp = "Eliminar"; break;
+      case "Taille maximum (pixel) de l'imagette" : $tmp = "El tamaño máximo (pixel) para thumb"; break;
+      case "Taille maximum d'un fichier : " : $tmp = "Tamaño máximo de fichero : "; break;
+      case "Temps de cache (en seconde) des imagettes" : $tmp = "Duración de la caché (en segundos) para thumbs"; break;
+      case "Type de fichier interdit" : $tmp = "Tipo de fichero no permitido"; break;
+      case "Vous n'êtes pas autorisé à utiliser le gestionnaire de média. SVP contacter l'administrateur." : $tmp = "No está autorizado a utilizar el administrador de medios. Por favor contacte al administrador."; break;
 
-   default: $tmp = "Requiere una traducción [** $phrase **]"; break;
- }
-return (htmlentities($tmp,ENT_QUOTES|ENT_SUBSTITUTE|ENT_HTML401,cur_charset));
+      default: $tmp = "Requiere una traducción [** $phrase **]"; break;
+   }
+   return (htmlentities($tmp,ENT_QUOTES|ENT_SUBSTITUTE|ENT_HTML401,cur_charset));
 }
 ?>
