5be14bd75c950a3bd80dd0bea7a93b028a1884af
jeanpierre <jean.barbary@wanadoo.fr>
2025-04-16 12:15:08 +0200

    more place for ip_visit_pag
    100 à 300 caractères (car parfois on peut avoir la data too long) à augmenter peut être encore un peu

--- revolution_16/sql/revolution_16.sql
+++ revolution_16/sql/revolution_16.sql
@@ -1086,7 +1086,7 @@ CREATE TABLE ip_loc (
   ip_id smallint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
   ip_long float NOT NULL DEFAULT '0',
   ip_lat float NOT NULL DEFAULT '0',
-  ip_visi_pag varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  ip_visi_pag varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   ip_visite mediumint(9) UNSIGNED NOT NULL DEFAULT '0',
   ip_ip varchar(54) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   ip_country varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
