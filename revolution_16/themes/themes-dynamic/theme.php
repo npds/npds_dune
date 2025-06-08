f997970a3134a26b17c3cbdf1cad2f9d6f510611
jeanpierre <jean.barbary@wanadoo.fr>
2025-06-08 17:19:11 +0200

    userpopover() no need self call !...
    https: //www.npds.org/viewtopic.php?topic=27006&forum=14
    Co-Authored-By: Nicolas2 <158532428+Nicolas2-dev@users.noreply.github.com>

--- revolution_16/themes/themes-dynamic/theme.php
+++ revolution_16/themes/themes-dynamic/theme.php
@@ -4,7 +4,7 @@
 /* ===========================                                          */
 /*                                                                      */
 /* DYNAMIC THEME engine for NPDS                                        */
-/* NPDS Copyright (c) 2002-2024 by Philippe Brunier                     */
+/* NPDS Copyright (c) 2002-2025 by Philippe Brunier                     */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
@@ -240,30 +240,28 @@ function userpopover($who,$dim,$avpop) {
             }
          }
       }
-   settype($ch_lat,'string');
-   $useroutils = '';
-   if($user or autorisation(-127)) {
-      if ($temp_user['uid']!= 1 and $temp_user['uid']!='')
-         $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="user.php?op=userinfo&amp;uname='.$temp_user['uname'].'" target="_blank" title="'.translate("Profil").'" ><i class="fa fa-lg fa-user align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Profil").'</span></a></li>';
-      if ($temp_user['uid']!= 1 and $temp_user['uid']!='')
-         $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="powerpack.php?op=instant_message&amp;to_userid='.urlencode($temp_user['uname']).'" title="'.translate("Envoyer un message interne").'" ><i class="far fa-lg fa-envelope align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Message").'</span></a></li>';
-      if ($temp_user['femail']!='')
-         $useroutils .= '<li><a class="dropdown-item  text-center text-md-start" href="mailto:'.anti_spam($temp_user['femail'],1).'" target="_blank" title="'.translate("Email").'" ><i class="fa fa-at fa-lg align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Email").'</span></a></li>';
-      if ($temp_user['uid']!= 1 and array_key_exists($ch_lat, $posterdata_extend)) {
-         if ($posterdata_extend[$ch_lat] !='')
-            $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="modules.php?ModPath=geoloc&amp;ModStart=geoloc&op=u'.$temp_user['uid'].'" title="'.translate("Localisation").'" ><i class="fas fa-map-marker-alt fa-lg align-middle fa-fw">&nbsp;</i><span class="ms-2 d-none d-md-inline">'.translate("Localisation").'</span></a></li>';
+      settype($ch_lat,'string');
+      $useroutils = '';
+      if($user or autorisation(-127)) {
+         if ($temp_user['uid']!= 1 and $temp_user['uid']!='')
+            $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="user.php?op=userinfo&amp;uname='.$temp_user['uname'].'" target="_blank" title="'.translate("Profil").'" ><i class="fa fa-lg fa-user align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Profil").'</span></a></li>';
+         if ($temp_user['uid']!= 1 and $temp_user['uid']!='')
+            $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="powerpack.php?op=instant_message&amp;to_userid='.urlencode($temp_user['uname']).'" title="'.translate("Envoyer un message interne").'" ><i class="far fa-lg fa-envelope align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Message").'</span></a></li>';
+         if ($temp_user['femail']!='')
+            $useroutils .= '<li><a class="dropdown-item  text-center text-md-start" href="mailto:'.anti_spam($temp_user['femail'],1).'" target="_blank" title="'.translate("Email").'" ><i class="fa fa-at fa-lg align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Email").'</span></a></li>';
+         if ($temp_user['uid']!= 1 and array_key_exists($ch_lat, $posterdata_extend)) {
+            if ($posterdata_extend[$ch_lat] !='')
+               $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="modules.php?ModPath=geoloc&amp;ModStart=geoloc&op=u'.$temp_user['uid'].'" title="'.translate("Localisation").'" ><i class="fas fa-map-marker-alt fa-lg align-middle fa-fw">&nbsp;</i><span class="ms-2 d-none d-md-inline">'.translate("Localisation").'</span></a></li>';
+         }
       }
-   }
-   if ($temp_user['url']!='')
-      $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="'.$temp_user['url'].'" target="_blank" title="'.translate("Visiter ce site web").'"><i class="fas fa-external-link-alt fa-lg align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visiter ce site web").'</span></a></li>';
-   if ($temp_user['mns'])
-       $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="minisite.php?op='.$temp_user['uname'].'" target="_blank" title="'.translate("Visitez le minisite").'" ><i class="fa fa-lg fa-desktop align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visitez le minisite").'</span></a></li>';
-
-   if (stristr($temp_user['user_avatar'],'users_private')) 
-      $imgtmp=$temp_user['user_avatar'];
-   else
-      if ($ibid=theme_image('forum/avatar/'.$temp_user['user_avatar'])) {$imgtmp=$ibid;} else {$imgtmp='images/forum/avatar/'.$temp_user['user_avatar'];}
-
+      if ($temp_user['url']!='')
+         $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="'.$temp_user['url'].'" target="_blank" title="'.translate("Visiter ce site web").'"><i class="fas fa-external-link-alt fa-lg align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visiter ce site web").'</span></a></li>';
+      if ($temp_user['mns'])
+         $useroutils .= '<li><a class="dropdown-item text-center text-md-start" href="minisite.php?op='.$temp_user['uname'].'" target="_blank" title="'.translate("Visitez le minisite").'" ><i class="fa fa-lg fa-desktop align-middle fa-fw"></i><span class="ms-2 d-none d-md-inline">'.translate("Visitez le minisite").'</span></a></li>';
+      if (stristr($temp_user['user_avatar'],'users_private')) 
+         $imgtmp=$temp_user['user_avatar'];
+      else
+         $imgtmp = theme_image('forum/avatar/'.$temp_user['user_avatar']) ?: 'images/forum/avatar/'.$temp_user['user_avatar'];
       $userpop = $avpop==1 ?
          '<img class="btn-outline-primary img-thumbnail img-fluid n-ava-'.$dim.' me-2" src="'.$imgtmp.'" alt="'.$temp_user['uname'].'" loading="lazy" />' :
          '
@@ -272,7 +270,9 @@ function userpopover($who,$dim,$avpop) {
                   <img class=" btn-outline-primary img-fluid n-ava-'.$dim.' me-0" src="'.$imgtmp.'" alt="'.$temp_user['uname'].'" loading="lazy" />
                </a>
                <ul class="dropdown-menu" data-bs-theme="light" >
-                  <li><span class="dropdown-item-text text-center py-0 my-0">'.userpopover($who,64,1).'</span></li>
+                  <li><span class="dropdown-item-text text-center py-0 my-0">
+                     <img class="btn-outline-primary img-thumbnail img-fluid n-ava-64 me-2" src="'.$imgtmp.'" alt="'.$temp_user['uname'].'" loading="lazy" />
+                  </span></li>
                   <li><h6 class="dropdown-header text-center py-0 my-0">'.$who.'</h6></li>
                   <li><hr class="dropdown-divider"></li>
                   '.$useroutils.'
@@ -280,8 +280,7 @@ function userpopover($who,$dim,$avpop) {
                   <li><div class="mx-auto text-center" style="max-width:170px;">'.$my_rs.'</div>
                </ul>
             </div>' ;
-
-   return $userpop;
+      return $userpop;
    }
 }
 ?>
